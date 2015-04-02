7877bfa935107a85e2f04ffef6f5196802689ff5
Ryan Zarick <rzarick@coderden.com>
2015-04-02 13:39:52 -0400

    Finance starter commit

---
+++ transaction/classes.py
@@ -0,0 +1,10 @@
+
+
+class AbstractTransaction (object):
+    """
+    This class is to be implemented by any of the financial
+    systems that require a transaction system. The class deals
+    with modifying transaction histories and balances for a
+    given currency type (points, dollars, bonus,etc).
+    """
+
