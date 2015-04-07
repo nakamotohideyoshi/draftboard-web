6b4ca321d06e6592b29051dbbdd6bdc4f25d72a0
Ryan Zarick <rzarick@coderden.com>
2015-04-06 21:45:34 -0400

    -Created the Cash app. -Wrote the withdrawal and deposit functionality for the Cash Transaction + test cases.

---
+++ cash/classes.py
@@ -0,0 +1,83 @@
+from cash.models import CashBalance, CashTransactionDetail
+from transaction.classes import AbstractTransaction
+from transaction.models import TransactionType
+from transaction.constants import TransactionTypeConstants
+from cash.exceptions import OverdraftException
+from dfslog.classes import Logger, ErrorCodes
+class CashTransaction(AbstractTransaction):
+    """
+    Implements the :class:`transaction.classes.AbstractTransaction` class.
+    This class handles all dealings with accounting for managing the cash
+    finances.
+    """
+    def __init__(self, user):
+        super().__init__(user)
+        self.transaction_detail_class = CashTransactionDetail
+        self.balance_class = CashBalance
+        self.accountName = "cash"
+
+    def check_sufficient_funds(self, amount):
+        """
+        Check to see if the user has enough funds
+        :param amount: the amount to check against the user's account
+        :return: True if the user has enough funds
+        """
+        cash_balance = self.get_balance_amount()
+        if(cash_balance < amount):
+            return False
+        return True
+
+
+    def withdraw(self, amount):
+        """
+        Creates a Withdraw in the user's Cash account.
+
+        :param amount: The dollar amount that is being removed from the account.
+            This should be a positive number.
+
+        :raises :class:`cash.exceptions.OverdraftException`: When
+            the user does not have enough cash for the withdrawal
+        :raises :class:`transaction.exceptions.AmountNegativeException`:
+            When the amount is a negative number.
+        """
+
+        #
+        # validates the amount is positive
+        self.validate_amount(amount)
+
+        #
+        # Validate the user has the funds for the withdrawal
+        if(not self.check_sufficient_funds( amount)):
+            raise OverdraftException(self.user.username)
+
+        #
+        #creates the transaction
+        category = TransactionType.objects.get(pk=TransactionTypeConstants.CashWithdrawal.value)
+        #
+        # makes the amount negative because it is a withdrawal
+        self.create(category,-amount)
+        Logger.log(ErrorCodes.INFO,"Withdrawal", self.user.username+" withdrew $"+str(amount)+" from their cash account.")
+
+
+    def deposit(self, amount):
+        """
+        Creates a Deposit in the user's Cash account.
+
+        :param user: The user the cash is being added to.
+        :param amount: The dollar amount that is being added to the account.
+
+        :raises :class:`transaction.exceptions.AmountNegativeException`:
+            When the amount is a negative number.
+        """
+        #
+        # validates the amount is positive
+        # validates the amount is positive
+        self.validate_amount(amount)
+
+        #
+        #creates the transaction
+        category = TransactionType.objects.get(pk=TransactionTypeConstants.CashDeposit.value)
+        self.create(category,amount)
+        Logger.log(ErrorCodes.INFO, "Deposit", self.user.username+" deposited $"+str(amount)+" into their cash account.")
+
+
---
+++ cash/models.py
@@ -0,0 +1,15 @@
+from transaction.models import TransactionDetail, Balance
+
+class CashBalance(Balance):
+    """
+    Implements the :class:`transaction.models.Balance` model.
+    """
+    pass
+
+class CashTransactionDetail(TransactionDetail):
+    """
+    Implements the :class:`transaction.models.TransactionDetail` model.
+    """
+    pass
+
+
