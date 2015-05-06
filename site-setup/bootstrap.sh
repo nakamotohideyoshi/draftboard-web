fee57a4f9abfabdecabc6c44a3434eae57921400
cbanister <cbanister@coderden.com>
2015-05-05 20:51:17 -0400

    fixed Vagrantfile and bootstrap to work with 64 bit VMs. installs java 7 and mongo as well.

--- site-setup/bootstrap.sh
+++ site-setup/bootstrap.sh
@@ -6,54 +6,87 @@
 ### 2.  use 'python3' not 'python'    !!
 ### 3. 	this script executes from /home/vagrant 	but your code should be in /vagrant !
 ### 4. 	"-y" flag to apt-get commands automatically says 'y' to prompts
+###
+###     Everything here was tested using:
+###     $> VBoxManage -v
+###     4.3.26r98988
+###
+###     $> vagrant --version
+###     Vagrant 1.7.2
+###
+###     $> mongod --version
+###     db version v2.4.9
+###
 ##############################################################################
 
-apt-get update
-#apt-get install -y vim 	# already installed on base VM
+
+#
+######################################################################
+#     				MongoDB
+#
+# If we ever move to using a 64 bit version of mongo for the VM:
+# http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+######################################################################
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get udpate
+### apt-get install -y mongodb-org   ### install latest.   the speficic version install is on the next line
+apt-get install -y mongodb-org=3.0.2 mongodb-org-server=3.0.2 mongodb-org-shell=3.0.2 mongodb-org-mongos=3.0.2 mongodb-org-tools=3.0.2
+
+#cp site-setup/mongodb.conf /etc/mongodb.conf       # make it use our conf file
 
 #
 #################################################################
-#     				db setup
+#     				Java
 #################################################################
+# install oracle java 7 - we wil need this for DataDen primarily and the VM doesnt come with it
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get install -y oracle-java7-installer
+java -version    # output the version
+
+#
+######################################################################
+#     				Postgres
+######################################################################
 apt-get install -y postgresql
 
 # Remove password from default postgres user
-sudo -u postgres psql template1 -x -c "ALTER ROLE postgres PASSWORD ''"
-# Set postgres configuration
-cp /vagrant/site-setup/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
-# Since we only use this box for development, open up all connections to the world.
-cp /vagrant/site-setup/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-# Restart postgres to reload configurations
-service postgresql restart
+sudo -u postgres psql template1 -x -c "ALTER ROLE postgres PASSWORD '';"
+cd /vagrant
+./site-setup/find_pg_hba.sh
+service postgresql restart                  # restart postgres
+./site-setup/createuser_vagrant.sh          # great the 'vagrant' user in postgres
 
+#
+# it will be useful to have git on the machine
 apt-get install -y git
 
 #
-# create postgres user 'vagrant'
-cd /vagrant
-./site-setup/createuser_vagrant.sh
-
-#
-# create the 'dfs' database, and give 'vagrant' the necessary permissions for postgres stuff
-./site-setup/create_db_dfs.sh      # perform the last 4 commands
-
-#
-# Install redis + add configuration file.
+######################################################################
+#     				Redis
+######################################################################
 apt-get install -y redis-server=2:2.8.4-2
 cp /vagrant/site-setup/redis.conf /etc/redis/redis.conf
 
+# start mongod daemon
+# $> sudo service mongod start         # this may work, but we need to configure a replica set
+mkdir /data
+mkdir /data/db
+#       cp /vagrant/site-setup/mongodb.conf /etc/mongodb/mongodb.conf
+#       service mongod restart
 
-#
-#apt-get install -y python-setuptools 		# a dependency for many python things
+# setuptools then pip - were using python3 for this project
 apt-get install -y python3-setuptools 		# a dependency for many python things
-
-#apt-get install -y python-pip
 apt-get install -y python3-pip 				# installs 'pip3' utility
 
+# this global package will be required before you can use pip3 to install psycopg2
+apt-get build-dep -y psycopg2
 
-################### after you have pip3, you can install virtual env and set it up #############
-
-#
 # install virtualenv (should always do this for python projects)
 pip3 install virtualenv
 
@@ -71,64 +104,42 @@ chown -R vagrant:vagrant /home/vagrant/venv
 source venv/bin/activate
 
 #
-# this global package will be required before you can use pip3 to install psycopg2
-apt-get build-dep -y psycopg2
+# install django and everything in requirements/local.txt
+venv/bin/pip3 install -r /vagrant/requirements/local.txt
 
 #
-# install django and the basic things it needs...
-    #<<<<<<< HEAD
-    #venv/bin/pip3 install -r /vagrant/requirements/local.txt
-    #
-    ## Add an environment variable to tell django to always use the local settings by default.
-    #echo "export DJANGO_SETTINGS_MODULE=mysite.settings.local" >> /home/vagrant/.bashrc
-    #
-    ## Activate the virtualenv on every login
-    #echo "source venv/bin/activate" >> /home/vagrant/.bashrc
-    #=======
-venv/bin/pip3 install Django==1.8               # LTS April 1, 2015 (no foolin!)
-venv/bin/pip3 install psycopg2
-venv/bin/pip3 install gunicorn
-venv/bin/pip3 install dj-database-url
-venv/bin/pip3 install dj-static
-venv/bin/pip3 install static
-venv/bin/pip3 install djangorestframework    	# optional - for creating apis
-venv/bin/pip3 install markdown 					# optional - for creating apis
-venv/bin/pip3 install django-filter 			# optional - for creating apis
-venv/bin/pip3 install sphinx 				    # !?  install in virtualenv. maybe do system-wide??
-venv/bin/pip3 install braintree                 # payment processing
-#venv/bin/pip3 install django_braintree          # third party braintree integration we might use
-venv/bin/pip3 install django-suit
-venv/bin/pip3 install django-braces
-venv/bin/pip3 install testfixtures
+# we will need this for ssl stuff (and potentially python paypal sdk)
+apt-get install -y libssl-dev libffi-dev
 
-venv/bin/pip3 install unipath
-venv/bin/pip3 install raven
-venv/bin/pip3 install django_extensions
-venv/bin/pip3 install django-pipeline
-venv/bin/pip3 install django-debreach
-venv/bin/pip3 install whitenoise
-
-#
-# we will need this for ssl stuff (including python paypal sdk)
-apt-get install libssl-dev libffi-dev
-#venv/bin/pip3 install paypalrestsdk
-
-#apt-get install redis-server           #installed above
-venv/bin/pip3 install django-redis
-venv/bin/pip3 install django-celery
-    #>>>>>>> 30e843f782ed8651ae0e562d73221396a1afc0eb
+cd /vagrant
 
 #
 # we will need a webserver of course, and drop in our nginx server conf file & restart
 apt-get install -y nginx
-cp /vagrant/site-setup/nginx-default /etc/nginx/sites-available/default        # websrv/nginx-default is a simple server{}
+
+# copy over a simple config that will enable us to hit django's runserver on localhost:8888
+cp /vagrant/site-setup/nginx-default /etc/nginx/sites-available/default
 service nginx restart
 
-    #<<<<<<< HEAD
-    ## TODO: these still fail for me, not quite sure why, something about django's database configuration.
-    #=======
-    ##
-    ## switch to user 'vagrant' because manage.py doesnt like connecting to postgres otherwise
-    #>>>>>>> 30e843f782ed8651ae0e562d73221396a1afc0eb
+### get back to the project root before the following commands
 cd /vagrant
-./site-setup/migrate_and_syncdb.sh
+
+#
+# we will need a webserver of course, and drop in our nginx server conf file & restart
+apt-get install -y nginx
+
+# copy over a simple config that will enable us to hit django's runserver on localhost:8888
+cp /vagrant/site-setup/nginx-default /etc/nginx/sites-available/default
+service nginx restart
+
+#
+# Add an environment variable to tell django to always use the local settings by default
+echo "export DJANGO_SETTINGS_MODULE=mysite.settings.local" >> /home/vagrant/.bashrc
+
+#
+# Activate the virtualenv on every login
+echo "source venv/bin/activate" >> /home/vagrant/.bashrc
+
+#
+# migrate - ie: have django create all the db tables. (will install custom fixtures in migration files)
+./migrate.sh
