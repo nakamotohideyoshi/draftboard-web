8144f60d0e5ec084569c6511c185a8291d3e80f8
Ryan Zarick <rzarick@coderden.com>
2015-05-19 12:11:37 -0400

    API for viewing active tickets

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Ticket, TicketAmount
+from .serializers import TicketSerializer
+from .classes import TicketManager
+class TicketAvailableListAPIView(generics.GenericAPIView):
+    """
+    Gets the cash balance as a string for the logged in user formatted like '$5.50'.
 
-# Create your views here.
+        * |api-text| :dfs:`cash/balance/`
+
+
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, format=None):
+
+        user = self.request.user
+        tm = TicketManager(user)
+        tickets = tm.get_available_tickets()
+        serializer = TicketSerializer(tickets, many=True)
+        return Response(serializer.data)
+
