4fcafbeeda827691e7042d92f7b7b8eb1f33d6f0
cbanister <cbanister@coderden.com>
2015-05-28 16:57:33 -0400

    added sports.models.Position

--- sports/migrations/0002_sitesport.py
+++ sports/migrations/0002_sitesport.py
@@ -3,6 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+def load_initial_data(apps, schema_editor):
+    """
+    Loads the initial WithdrawStatus(s). This function will be passed to 'migrations.RunPython' which supplies the arguments.
+
+    :param apps:
+    :param schema_editor:
+    :return:
+    """
+    #
+    # get the model by name
+    sports = ['nfl','mlb','nba','nhl']
+    SiteSport = apps.get_model('sports', 'SiteSport')
+    for sport in sports:
+        model = SiteSport()
+        model.name = sport
+        model.save()
 
 class Migration(migrations.Migration):
 
@@ -19,4 +35,8 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=128)),
             ],
         ),
+
+        #
+        # additionally, run function to load the initial objects
+        migrations.RunPython( load_initial_data )
     ]
