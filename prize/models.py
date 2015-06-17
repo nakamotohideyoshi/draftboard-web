0ce610979e6663c3044a75770829e49c7791b34a
cbanister <cbanister@coderden.com>
2015-06-17 16:46:36 -0400

    prize structure creators to back the gui

--- prize/models.py
+++ prize/models.py
@@ -2,6 +2,22 @@
 # prize/models.py
 
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+from transaction.models import AbstractAmount
+from cash.classes import CashTransaction
+
+class GeneratorSettings( models.Model ):
+    buyin           = models.IntegerField(default=0, null=False)    # label='the amount of the buyin')
+    first_place     = models.IntegerField(default=0, null=False)    # label='value ($) of first place as integer')
+    round_payouts   = models.IntegerField(default=0, null=False)    # label='each ranks prize must be a multiple of this integer value')
+    payout_spots    = models.IntegerField(default=0, null=False)    # total # of prizes
+    prize_pool      = models.IntegerField(default=0, null=False)    # total prize pool
+
+    def __str__(self):
+        return 'buyin: %s, first_place: %s, round_payouts: %s, payout_spots: %s, prize_pool: %s' % \
+                (self.buyin, self.first_place, self.round_payouts, self.payout_spots, self.prize_pool)
 
 class PrizeStructure( models.Model ):
     """
@@ -10,91 +26,35 @@ class PrizeStructure( models.Model ):
     """
 
     created = models.DateTimeField(auto_now_add=True)
+    name    = models.CharField(max_length=128, default='', null=False, blank=True)
 
-    name            = models.CharField(max_length=128, default='', null=False, blank=True)
-    buyin           = models.IntegerField(default=0, null=False)    # label='the amount of the buyin')
-    first_place     = models.IntegerField(default=0, null=False)    # label='value ($) of first place as integer')
-    round_payouts   = models.IntegerField(default=0, null=False)    # label='each ranks prize must be a multiple of this integer value')
-    payout_spots    = models.IntegerField(default=0, null=False)    # total # of prizes
-    prize_pool      = models.IntegerField(default=0, null=False)    # total prize pool
+    generator = models.ForeignKey( GeneratorSettings, null=True )
 
     def __str__(self):
-        return '< %s | bi: %s, 1st: %s, rnd: %s, spots: %s, pp: %s)' % (
-                        self.name,
-                        self.buyin, self.first_place, self.round_payouts,
-                        self.payout_spots, self.prize_pool)
+        return '%s %s' % (self.__class__.__name__, self.name)
 
-    #class Meta:
-    #   we arent going to make it unique on all 5 things, but we probably should.
-    #   there just MAY be a situation in the future where we will want
-    #   multiple PrizeStructures with the same values ... no big deal.
-
-class AbstractPrize( models.Model ):
+class Rank( models.Model ):
     """
-    Abstract parent model which holds generic information about
-    the kind of Prize at a specific rank.
-    """
+    A rank is associated with a specific PrizeStructure and has a
+    generic foreign key to an amount model which may represent cash, or ticket prize
 
+    The GFK should always point to a transacation.models.AbstractAmount so
+    this models __str__ method works properly, as well
+    as the "_____PrizeStructureCreator" classes which build prize structures
+    """
     prize_structure = models.ForeignKey( PrizeStructure, null=False )
     rank            = models.IntegerField(default=0, null=False)
-    value           = models.FloatField(default=0, null=False)     # float in case of splitting prizes
+    amount_type     = models.ForeignKey(ContentType)
+    amount_id       = models.IntegerField()
+    amount          = GenericForeignKey( 'amount_type', 'amount_id' )
+
+    @property
+    def value(self):
+        """
+        wrapper to grab the amount instances 'amount' field ($ value)
+        """
+        return self.amount.amount
 
     def __str__(self):
-        return '< rank: %s, value: %s)' % (self.rank, self.value)
+        return '<%s | %s>' % (self.__class__.__name__, self.value)
 
-    class Meta:
-        abstract = True
-
-class AbstractActualPrize( models.Model ):
-    """
-    Abstract parent model which holds generic information about
-    the kind of Prize at a specific rank.
-    """
-
-    tied    = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = True
-
-class Cash( AbstractPrize ):
-    """
-    Represents a cash payout at a rank for a PrizeStructure
-    """
-
-    class Meta:
-        abstract = False # ensure this model gets migrated without any other fields
-        unique_together = ('prize_structure','rank')
-
-class Ticket( AbstractPrize ):
-    """
-    Represents a cash payout at a rank for a PrizeStructure
-    """
-
-    class Meta:
-        abstract = False # ensure this model gets migrated without any other fields
-        unique_together = ('prize_structure','rank')
-
-#
-# TODO - having a nice, perfect PrizeStructure is great. We have to have it to do payouts.
-#      However -- the actual payouts may include ties which can cause the ACTUAL payouts
-#                 to differ from the PrizeStructure. We may want to keep a history
-#                 of the actual payouts.
-class ActualCash( AbstractActualPrize ):
-    """
-    Represents a cash payout at a rank for a PrizeStructure
-    """
-
-    #tied    = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = False # ensure this model gets migrated without any other fields
-
-class ActualTicket( AbstractActualPrize ):
-    """
-    Represents a cash payout at a rank for a PrizeStructure
-    """
-
-    #tied    = models.BooleanField(default=False)
-
-    class Meta:
-        abstract = False # ensure this model gets migrated without any other fields
