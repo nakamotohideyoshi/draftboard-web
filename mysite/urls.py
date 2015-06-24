f51f57f2c434baecd64d3bf19b38d2c0621f35e0
cbanister <cbanister@coderden.com>
2015-06-24 14:43:10 -0400

    removed rest for testing

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,20 +12,20 @@ from rest_framework import routers, serializers, viewsets
 
 
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+# # Serializers define the API representation.
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('url', 'username', 'email', 'is_staff')
+#
+# # ViewSets define the view behavior.
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+# # Routers provide an easy way of automatically determining the URL conf.
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -46,11 +46,11 @@ urlpatterns = [
 
     #
     # this came with rest_framework
-    url(r'^', include(router.urls)),
-
+    # url(r'^', include(router.urls)),
     #
-    # this came with rest_framework
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # #
+    # # this came with rest_framework
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
 
