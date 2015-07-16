bde6a95ded839ed9568de7c02f74e91abfbf0834
Craig Ruks <craig@craigruks.com>
2015-07-16 15:44:14 -0600

    - Added in proof of concept for court live shooter - Updated settings to have split out nav and more templates built out

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,8 +5,11 @@ from frontend import views
 urlpatterns = patterns(
     '',
     (r'^frontend/layout/$', views.FrontendLayoutTemplateView.as_view()),
+    (r'^frontend/live/court/$', views.FrontendLiveCourtTemplateView.as_view()),
     (r'^frontend/styleguide/$', views.FrontendStyleGuideTemplateView.as_view()),
     (r'^frontend/lobby/$', views.FrontendLobbyTemplateView.as_view()),
     (r'^frontend/settings/$', views.FrontendSettingsTemplateView.as_view()),
     (r'^frontend/settings/transactions/$', views.FrontendSettingsTransactionHistoryTemplateView.as_view()),
+    (r'^frontend/settings/deposits/$', views.FrontendSettingsDepositsTemplateView.as_view()),
+    (r'^frontend/settings/withdraws/$', views.FrontendSettingsWithdrawsTemplateView.as_view()),
 )
---
+++ static/src/js/components/live-nba-court/live-nba-court-shooter.jsx
@@ -0,0 +1,87 @@
+"use strict";
+
+var React = require('react');
+var renderComponent = require('lib/render-component');
+
+/**
+ * The score ticker on the top of the page.
+ */
+var LiveNBAShooter = React.createClass({
+  getInitialState: function() {
+    // example coordinates from the API
+    var xCoord = 350;
+    var yCoord = 350;
+
+    // width and height of image
+    var imgWidth = 2003;
+    var imgHeight = 495;
+
+    // left backcourt x coordinates
+    var xTopLeft = 358;
+
+    // right backcourt x coordinates
+    var xTopRight = 1645;
+
+    // sidecourts y coordinates
+    var yTop = 45;
+    var yBottom = 350;
+
+    // distance between the two sidecourts
+    var yHeight = yBottom - yTop;
+
+    // API max distances, goes from 0 to max
+    var xAPIMax = 1128;
+    var yAPIMax = 600;
+
+    // first we convert these into percentages (as decimals)
+    var xPercent = (xCoord * (100 / xAPIMax)) / 100;
+    var yPercent = (yCoord * (100 / yAPIMax)) / 100;
+
+    // then we figure out where it is height wise on the trapezoid
+    // since the top and bottom are parallel we can always figure out the height
+
+    var transposedY = yPercent * yHeight;
+    // and from this we can derive the final position from the top
+    var finalTop = (transposedY + yTop) / imgHeight;
+
+    // since this is a 45 deg trapezoid, we can deduce that the distance to the left will be the same as top
+
+    // now that we know the transposedY, we can figure out what the x left and x right are and find the transposedXLength
+    var transposedXLeft = xTopLeft - transposedY;
+    var transposedXRight = xTopRight + transposedY;
+    var transposedXLength = transposedXRight - transposedXLeft;
+
+    // and now that we have the length we can find the final left position of the shooter
+    var finalLeft = (transposedXLeft + (xPercent * transposedXLength)) / imgWidth;
+
+    // convert from decimal to CSS percentage, and round to nearest hundredth
+    finalTop = Math.ceil(finalTop * 10000) / 100;
+    finalLeft = Math.ceil(finalLeft * 10000) / 100;
+
+    return {
+      finalLeft: finalLeft,
+      finalTop: finalTop
+    };
+  },
+
+  render: function() {
+    var shooterPositionStyle = {
+      left: this.state.finalLeft + '%',
+      top: this.state.finalTop + '%'
+    };
+
+    return (
+      <div className='shooter-position' style={shooterPositionStyle}>
+        <div className='shooter-centered'></div>
+      </div>
+    );
+  }
+
+});
+
+
+// Render the component.
+renderComponent(<LiveNBAShooter />, '.live-nba-court');
+
+
+module.exports = LiveNBAShooter;
