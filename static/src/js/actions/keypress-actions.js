35a8283efbd85fade9ff8d27da9ec2cf9c64ee19
Zachary Wood <me@zacharywood.com>
2015-07-16 13:27:37 -0600

    keypress-actions test

--- static/src/js/actions/keypress-actions.js
+++ static/src/js/actions/keypress-actions.js
@@ -23,10 +23,14 @@ var keyCodeMap = {
 
 document.onkeyup = function (e) {
   e = e || window.event;
+  // If for whatever eason the browser doesn't give us an activeElement, assume body.
+  var activeElement = document.activeElement || document.body;
+
   // Check that the user isn't focused in an ignored element, and that the key pressed
   // is in our map.
-  if (ignoredElements.indexOf(document.activeElement.nodeName) === -1 &&
-      keyCodeMap[e.keyCode] !== undefined) {
+  if (ignoredElements.indexOf(activeElement.nodeName) === -1 &&
+      keyCodeMap[e.keyCode] !== undefined
+    ) {
     // Fire the corresponding reflux action.
     KeypressActions['keypress' + keyCodeMap[e.keyCode]].apply();
   }
