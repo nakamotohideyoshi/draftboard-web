5f8beb21ade37fb2f7a99d1952fca70a4139b60a
cbanister <cbanister@coderden.com>
2015-08-19 13:20:01 -0400

    apis     contest lobby, draft group, lineup create, lineup views

--- draftgroup/urls.py
+++ draftgroup/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns( '',
 
     #
     # get the contests for display on the main contest lobby
-    (r'^get/$', DraftGroupAPIView.as_view()),
+    (r'^(?P<pk>[0-9]+)$', DraftGroupAPIView.as_view()),
 
 )
--- lineup/serializers.py
+++ lineup/serializers.py
@@ -3,6 +3,7 @@
 
 from rest_framework import serializers
 from lineup.models import Lineup, Player
+import draftgroup.models
 
 class LineupSerializer(serializers.ModelSerializer):
 
@@ -18,3 +19,32 @@ class PlayerSerializer(serializers.ModelSerializer):
         model = Player
         fields = ('player_id', 'full_name', 'lineup', 'roster_spot', 'idx')
 
+class CreateLineupSerializer(serializers.Serializer):
+
+    draft_group = serializers.IntegerField()
+
+    players = serializers.ListField(
+        child=serializers.IntegerField(min_value=0, max_value=9999999)
+    )
+
+    def validate(self, data):
+        """
+        Check that the start is before the stop.
+        """
+        draft_group_id = data['draft_group']
+        if draft_group_id is None:
+            raise serializers.ValidationError("invalid draft_group id")
+        try:
+            draftgroup.models.DraftGroup.objects.get(pk=draft_group_id)
+        except draftgroup.models.DraftGroup.DoesNotExist:
+            raise serializers.ValidationError('invalid draft_group id')
+
+        return data
+
+class EditLineupSerializer(serializers.Serializer):
+
+    lineup = serializers.IntegerField()
+
+    players = serializers.ListField(
+        child=serializers.IntegerField(min_value=0, max_value=9999999)
+    )
