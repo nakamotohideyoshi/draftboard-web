fdcd8ce9849adfc42be6c0d980587ab6bae5ddec
cbanister <cbanister@coderden.com>
2015-09-21 19:33:35 -0400

    validation for arguments to process card payment

--- optimal_payments/tests.py
+++ optimal_payments/tests.py
@@ -1,3 +1,16 @@
+#
+# optimal_payments/tests.py
+
 from django.test import TestCase
 
-# Create your tests here.
+class CardPaymentTest(TestCase):
+
+    def test_amount_fractional_pennies(self):
+        pass # TODO
+
+    def test_amount_invalid_type(self):
+        pass # TODO
+
+    def test_amount_unparseable_value(self):
+        pass # TODO
+
