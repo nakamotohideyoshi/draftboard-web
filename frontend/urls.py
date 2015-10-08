66de6ecc9c23b98c652204e59cb50814e649c972
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2015-10-08 13:51:49 -0600

    Add a homepage template + url.

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,11 +4,12 @@ from frontend import views
 
 urlpatterns = patterns(
     '',
+    (r'^$', views.FrontendHomepageTemplateView.as_view()),
     (r'^frontend/layout/$', views.FrontendLayoutTemplateView.as_view()),
     (r'^frontend/tooltip/$', views.FrontendTooltipTemplateView.as_view()),
     (r'^frontend/live/$', views.FrontendLiveTemplateView.as_view()),
     (r'^frontend/styleguide/$', views.FrontendStyleGuideTemplateView.as_view()),
-    (r'^frontend/lobby/$', views.FrontendLobbyTemplateView.as_view()),
+    (r'^lobby/$', views.FrontendLobbyTemplateView.as_view()),
     (r'^frontend/settings/$', views.FrontendSettingsTemplateView.as_view()),
     (r'^frontend/settings/transactions/$', views.FrontendSettingsTransactionHistoryTemplateView.as_view()),
     (r'^frontend/settings/deposits/$', views.FrontendSettingsDepositsTemplateView.as_view()),
