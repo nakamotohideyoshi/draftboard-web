a67380a0f23e37709ea22a699bd3dc53c9c6453a
cbanister <cbanister@coderden.com>
2015-11-10 13:42:08 -0500

    payment_token, credit card deposit

--- optimal_payments/urls.py
+++ optimal_payments/urls.py
@@ -3,7 +3,8 @@
 
 from django.conf.urls import patterns
 from .views import AddPaymentMethodAPIView, PaymentMethodAPIView, \
-                    RemovePaymentMethodAPIView
+                    RemovePaymentMethodAPIView, \
+                    DepositPaymentTokenAPIView, DepositCreditCardAPIView
 
 urlpatterns = patterns(
     '',
@@ -11,9 +12,18 @@ urlpatterns = patterns(
     # add a payment method
     (r'^add/$', AddPaymentMethodAPIView.as_view()),
 
-    #
+    # remove a deposit method
     (r'^remove/$', RemovePaymentMethodAPIView.as_view()),
 
+    #
+    # make a deposit with a saved payment method
+    (r'^deposit/method/$', DepositPaymentTokenAPIView.as_view()),
+
+    #
+    # make a deposit with a credit card
+    (r'^deposit/cc/$', DepositCreditCardAPIView.as_view()),
+
+    #
     # get a list of the payment methods for this user
     (r'^$', PaymentMethodAPIView.as_view()),
 
