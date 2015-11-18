5ca3bb7a1525ddea48a10116901f5dacef8c83fa
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2015-11-17 22:58:55 -0700

    fix test coverage reports.

--- static/src/js/test/load-all-modules-test.js
+++ static/src/js/test/load-all-modules-test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * This is really stupid... Unless you're using Gulp + a plugin, Istanbul doesn't automatically
  * see all of your files to determine the test coverage of them, it only looks at the ones that
@@ -7,11 +5,13 @@
  * require them. This uses the require-dir module to load in all of our files.
  */
 
-// disable this until we get thing sorted out.
 
-// var requireDir = require('require-dir');
-//
-// requireDir('../actions', {recurse: true});
-// requireDir('../components', {recurse: true});
-// requireDir('../lib', {recurse: true});
-// requireDir('../stores', {recurse: true});
+var requireDir = require('require-dir');
+
+requireDir('../actions', {recurse: true});
+requireDir('../components', {recurse: true});
+requireDir('../lib', {recurse: true});
+requireDir('../selectors', {recurse: true});
+requireDir('../reducers', {recurse: true});
+requireDir('../simulations', {recurse: true});
+requireDir('../middleware', {recurse: true});
