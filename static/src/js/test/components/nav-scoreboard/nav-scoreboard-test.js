fc82d4bf482b10d051c661e9f60352a311c264c7
ivo- <ivailo.nikolaev.hristov@gmail.com>
2015-11-24 14:00:54 +0200

    Add nav-scoreboard redux selector and integration

---
+++ static/src/js/test/components/nav-scoreboard/nav-scoreboard-test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+require('../../test-dom')();
+const React = require('react');
+const ReactDOM = require('react-dom');
+const NavScoreboard = require('../../../components/nav-scoreboard/nav-scoreboard.jsx');
+const expect = require('chai').expect;
+
+const {TYPE_SELECT_GAMES, TYPE_SELECT_LINEUPS} = NavScoreboard;
+
+describe("NavScoreboard Component", function() {
+
+  beforeEach(function(done) {
+    var self = this;
+    document.body.innerHTML = '';
+    // The DOM element that the component will be rendered to.
+    this.targetElement = document.body.appendChild(document.createElement('div'));
+    // Render the component into our fake jsdom element.
+    this.component = ReactDOM.render(
+      <NavScoreboard />,
+      this.targetElement,
+      function() {
+        // Once it has been rendered...
+        // Grab it from the DOM.
+        self.element = ReactDOM.findDOMNode(this);
+        done();
+      }
+    );
+  });
+
+  afterEach(function() {
+    document.body.innerHTML = '';
+  });
+
+  it('should render a div tag, menu, user info, filters, slider and logo', function() {
+    expect(this.element.tagName).to.equal('DIV');
+    expect(
+      this.element.querySelectorAll('.cmp-nav-scoreboard--menu').length
+    ).to.equal(1);
+    expect(
+      this.element.querySelectorAll('.cmp-nav-scoreboard--user-info').length
+    ).to.equal(1);
+    expect(
+      this.element.querySelectorAll('.cmp-nav-scoreboard--filters').length
+    ).to.equal(1);
+    expect(
+      this.element.querySelectorAll('.cmp-nav-scoreboard--slider').length
+    ).to.equal(1);
+    expect(
+      this.element.querySelectorAll('.cmp-nav-scoreboard--logo').length
+    ).to.equal(1);
+  });
+
+  it("should select and render the first filters option", function()  {
+    expect(
+      this.component.state.selectedOption
+    ).to.equal(this.component.getSelectOptions()[0].option);
+
+    if (this.component.state.selectedType == TYPE_SELECT_LINEUPS) {
+      expect(
+        this.element.querySelectorAll('.cmp-nav-scoreboard--lineups-list').length
+      ).to.equal(1);
+    } else if (this.component.state.selectedType == TYPE_SELECT_GAMES) {
+      expect(
+        this.element.querySelectorAll('.cmp-nav-scoreboard--games-list').length
+      ).to.equal(1);
+    } else {
+      new Error("Selected nav-scoreboard filter is not rendered.");
+    }
+  });
+
+});
