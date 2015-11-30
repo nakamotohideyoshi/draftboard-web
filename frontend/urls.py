a2c1641533d7642320c748ed10c0efb3fe962727
Craig Ruks <craig@craigruks.com>
2015-11-30 16:10:02 -0700

    Can now navigate around the site

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,16 +5,12 @@ from frontend import views
 urlpatterns = patterns(
     '',
     (r'^$', views.FrontendHomepageTemplateView.as_view()),
-    (r'^frontend/layout/$', views.FrontendLayoutTemplateView.as_view()),
-    (r'^frontend/tooltip/$', views.FrontendTooltipTemplateView.as_view()),
-    (r'^frontend/live/$', views.FrontendLiveTemplateView.as_view()),
-    (r'^frontend/styleguide/$', views.FrontendStyleGuideTemplateView.as_view()),
-    (r'^lobby/$', views.FrontendLobbyTemplateView.as_view()),
-    (r'^frontend/results/$', views.FrontendResultsTemplateView.as_view()),
-    (r'^frontend/settings/$', views.FrontendSettingsTemplateView.as_view()),
-    (r'^frontend/settings/transactions/$', views.FrontendSettingsTransactionHistoryTemplateView.as_view()),
-    (r'^frontend/settings/deposits/$', views.FrontendSettingsDepositsTemplateView.as_view()),
-    (r'^frontend/settings/withdraws/$', views.FrontendSettingsWithdrawsTemplateView.as_view()),
     (r'^draft/(?P<draft_group_id>[0-9]+)/$', views.FrontendDraftTemplateView.as_view()),
-    (r'^frontend/pane/$', views.FrontendPaneTemplateView.as_view())
+    (r'^live/$', views.FrontendLiveTemplateView.as_view()),
+    (r'^lobby/$', views.FrontendLobbyTemplateView.as_view()),
+    (r'^results/$', views.FrontendResultsTemplateView.as_view()),
+    (r'^settings/$', views.FrontendSettingsTemplateView.as_view()),
+    (r'^settings/transactions/$', views.FrontendSettingsTransactionHistoryTemplateView.as_view()),
+    (r'^settings/deposits/$', views.FrontendSettingsDepositsTemplateView.as_view()),
+    (r'^settings/withdraws/$', views.FrontendSettingsWithdrawsTemplateView.as_view()),
 )
