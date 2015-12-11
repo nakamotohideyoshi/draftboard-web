b7fad8acfa68d60bdaf6a7581d46bbfffef3b405
Zachary Wood <me@zacharywood.com>
2015-12-10 18:44:37 -0700

    Sparkline graphs + draft list styles.

--- static/src/js/components/draft/draft-player-list-row.jsx
+++ static/src/js/components/draft/draft-player-list-row.jsx
@@ -1,7 +1,7 @@
 var React = require('react')
-// var DraftActions = require("../../actions/draft-actions");
 import * as AppActions from '../../stores/app-state-store.js'
 var log = require('../../lib/logging.js')
+import Sparkline from './sparkline.jsx'
 
 
 /**
@@ -58,19 +58,23 @@ var DraftPlayerListRow = React.createClass({
         className={classes}
         onClick={this.onRowClick.bind(this, this.props.row.player_id)}
       >
-        <td>{this.props.row.position}</td>
-        <td>👤</td>
-        <td>{this.props.row.name} / {this.props.row.team_alias}</td>
-        <td className="status">{this.props.row.status}</td>
-        <td></td>
-        <td>{this.props.row.fppg.toFixed(1)}</td>
-        <td>${this.props.row.salary.toLocaleString('en')}</td>
         <td>
           <div
             className="button--mini button--gradient"
             onClick={this.onDraftClick.bind(this, this.props.row)}
             >Draft</div>
         </td>
+        <td className="position">{this.props.row.position}</td>
+        <td className="photo">👤</td>
+        <td className="name">
+          <span className="player">{this.props.row.name} </span>
+          <span className="team">{this.props.row.team_alias}</span>
+        </td>
+        <td className="status">{this.props.row.status}</td>
+        <td className="game"><span className="player-team">ME</span> @ YOU</td>
+        <td>{this.props.row.fppg.toFixed(1)}</td>
+        <td className="history"><Sparkline points={this.props.row.history} /></td>
+        <td className="salary">${this.props.row.salary.toLocaleString('en')}</td>
       </tr>
     );
   }
--- static/src/js/reducers/fantasy-history.js
+++ static/src/js/reducers/fantasy-history.js
@@ -6,8 +6,8 @@ const initialState = {}
 module.exports = function(state=initialState, action) {
   switch (action.type) {
 
-    case ActionTypes.FETCH_INJURIES_SUCCESS:
-      return Object.assign({}, state, action.body.injuries)
+    case ActionTypes.FETCH_FANTASY_HISTORY_SUCCESS:
+      return Object.assign({}, state, action.body.history)
 
     default:
       return state
