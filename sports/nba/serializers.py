5f47024c4328c76ebc60cc90850a8ddee514d28f
cbanister <cbanister@coderden.com>
2015-12-11 14:52:06 -0500

    added city name

--- sports/nba/serializers.py
+++ sports/nba/serializers.py
@@ -3,6 +3,7 @@
 
 from rest_framework import serializers
 from sports.serializers import InjurySerializer, FantasyPointsSerializer
+import sports.serializers
 from .models import Injury, PlayerStats, Team
 
 class InjurySerializer(InjurySerializer):
@@ -12,12 +13,23 @@ class InjurySerializer(InjurySerializer):
         model = Injury
         fields = ('iid', 'status','description','srid', 'comment', 'player_id')
 
-class TeamSerializer(InjurySerializer):
+class TeamSerializer(sports.serializers.TeamSerializer):
+
+    city = serializers.SerializerMethodField()
+    def get_city(self, team):
+        return team.market
 
     class Meta:
 
         model = Team
-        fields = ('id', 'srid', 'name', 'alias')
+        fields = sports.serializers.TeamSerializer.PARENT_FIELDS + ('city',)
+        # fields = (
+        #     # you shouldnt change id, srid, name, alias
+        #     'id', 'srid', 'name', 'alias',
+        #
+        #     # sport market/city just called city in this serializer
+        #     'city'
+        # )
 
 class FantasyPointsSerializer(FantasyPointsSerializer):
 
