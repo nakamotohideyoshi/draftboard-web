b18820ba1c6470f502f4a316ac834db12b5b650c
Zachary Wood <me@zacharywood.com>
2016-01-08 15:38:32 -0700

    "+" lineup button action in lobby sidebar

--- static/src/js/lib/utils.js
+++ static/src/js/lib/utils.js
@@ -6,14 +6,19 @@ import * as moment from 'moment'
  * @param {Integer} number
  */
 export function toTwoDigit(number) {
-  return (Math.abs(number) < 10) ? "0" + Math.abs(number) : Math.abs(number)
+  // Don't add a leading zero to negative numbers.
+  if (number < 0) {
+    return number
+  }
+
+  return (number < 10) ? "0" + number : number
 }
 
 
 /**
  * From a UTC timestamp, determine the time remaining, parsed out into hours, minutes, and seconds.
  * @param  {[type]} timestamp A UTC timestamp.
- * @return {Object}           Hours, Minutes and Seconds remaining until the timestamp.
+ * @return {Object} Hours, Minutes and Seconds remaining until the timestamp.
  */
 export function timeRemaining(timestamp) {
   // difference between when the contest starts and now (in ms).
@@ -21,9 +26,11 @@ export function timeRemaining(timestamp) {
   // convert to a moment 'duration' so we can parse it out.
   let duration = moment.duration(diffTime)
 
+  // The hours have no Math.abs because should be displayed as negative. (-32:05:57)
+  // A negative number means that the thing we're counting down to has past.
   return {
     hours: toTwoDigit(Math.floor(duration.asHours())),
-    minutes: toTwoDigit(duration.minutes()),
-    seconds: toTwoDigit(duration.seconds())
+    minutes: Math.abs(toTwoDigit(duration.minutes())),
+    seconds: Math.abs(toTwoDigit(duration.seconds()))
   }
 }
