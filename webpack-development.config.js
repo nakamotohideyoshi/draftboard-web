1662b80b77156cef4c46a87f6c7c88288b044315
Craig Ruks <craig@craigruks.com>
2016-01-09 13:12:52 -0600

    Organizing webpack config to be separated into dev and prod, and updating logging to not show on prod

--- webpack-development.config.js
+++ webpack-development.config.js
@@ -1,11 +1,50 @@
 'use strict';
-
+var path = require('path');
 var baseConfig = require('./webpack-base.config.js');
+var webpack = require('webpack');
 
 
-// Override base configuration entries with those specific to the webpack devserver.
-module.exports = baseConfig({
-  debug: true,
-  cache: true,
-  devtool: 'source-map'
-});
+// Override base configuration entries with those for the PRODUCTION build.
+var config = Object.assign(
+  baseConfig,
+  {
+    // An 'entry' is a compiled & bundled file.
+    entry: {
+      // Main app file.
+      'app': [
+        // Enable Webpack's dev server hot reloads for this entry.
+        'webpack/hot/dev-server',
+        path.join(__dirname, 'static', 'src', 'js', 'app.jsx')
+      ],
+      // unauthenticated users - they get a slimmed down set of static files.
+      'logged-out': [
+        // Enable Webpack's dev server hot reloads for this entry.
+        'webpack/hot/dev-server',
+        path.join(__dirname, 'static', 'src', 'js', 'app-logged-out.jsx')
+      ]
+    },
+    // Devserver config
+    devServer: {
+      contentBase: './build',
+      noInfo: false,
+      colors: true,
+      host: '0.0.0.0',
+      port: 8090,
+      stats: 'errors-only'
+    },
+    debug: true,
+    cache: true,
+    devtool: 'source-map'
+  }
+);
+
+
+config.plugins = config.plugins.concat([
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('debug')
+  }})
+])
+
+module.exports = config
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,22 +1,37 @@
 'use strict';
 var path = require('path');
 var baseConfig = require('./webpack-base.config.js');
+var webpack = require('webpack');
 
 
 // Override base configuration entries with those for the PRODUCTION build.
-module.exports = baseConfig({
-  debug: false,
-  // An 'entry' is a compiled & bundled file.
-  entry: {
-    // Main app file.
-    'app': [
-      // Our app to be bundled.
-      path.join(__dirname, 'static', 'src', 'js', 'app.jsx')
-    ],
-    // unauthenticated users - they get a slimmed down set of static files.
-    'logged-out': [
-      // Our app to be bundled.
-      path.join(__dirname, 'static', 'src', 'js', 'app-logged-out.jsx')
-    ]
+var config = Object.assign(
+  baseConfig,
+  {
+    debug: false,
+
+    // An 'entry' is a compiled & bundled file.
+    entry: {
+      // Main app file.
+      'app': [
+        // Our app to be bundled.
+        path.join(__dirname, 'static', 'src', 'js', 'app.jsx')
+      ],
+      // unauthenticated users - they get a slimmed down set of static files.
+      'logged-out': [
+        // Our app to be bundled.
+        path.join(__dirname, 'static', 'src', 'js', 'app-logged-out.jsx')
+      ]
+    }
   }
-});
+);
+
+config.plugins = config.plugins.concat([
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+  }})
+]);
+
+module.exports = config
