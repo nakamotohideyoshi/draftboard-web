f7087f34b7188aa3d3ea79329e7baec0d375e4e7
cbanister <cbanister@coderden.com>
2016-01-26 23:57:01 -0500

    flat cash prize structure

--- prize/exceptions.py
+++ prize/exceptions.py
@@ -7,3 +7,7 @@ class InvalidBuyinAndPrizePoolException(Exception):
     def __init__(self):
         super().__init__(
            "The prize pool must be evenly divisible by the buyin." )
+
+class RakeIsNot10PercentException(Exception): pass
+
+class NoMatchingTicketException(Exception): pass
