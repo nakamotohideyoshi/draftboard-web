cd17383a16875ea3d0756db55e732a3553c1af38
cbanister <cbanister@coderden.com>
2016-03-15 16:10:02 -0400

    fixed migrations for the four sports to handle existing Game models by setting a dummy season to make postgres null=False constraint happy

--- sports/nba/migrations/0004_auto_20160302_1121.py
+++ sports/nba/migrations/0004_auto_20160302_1121.py
@@ -3,6 +3,28 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+def load_initial_data(apps, schema_editor):
+    """
+    adds a dummy Season object to facillitate the migratino of older
+    Game objects which cant have a foreign key to a null Season!
+
+    :param apps:
+    :param schema_editor:
+    :return:
+    """
+
+    # get the model by name
+    sport = 'nba'
+    Season = apps.get_model(sport, 'Season')
+    try:
+        # this must exist for migration to work
+        season = Season.objects.get(pk=1)
+    except Season.DoesNotExist:
+        season = Season()
+        season.srid = '%s_dummy_srid' % sport
+        season.season_year = 0
+        season.season_type = '%s_dummy_season_type' % sport
+        season.save()
 
 class Migration(migrations.Migration):
 
@@ -15,12 +37,18 @@ class Migration(migrations.Migration):
             model_name='season',
             name='start_year',
         ),
-        migrations.AddField(
-            model_name='game',
-            name='season',
-            field=models.ForeignKey(to='nba.Season', default=1),
-            preserve_default=False,
-        ),
+
+        # # add dummy/initial Season
+        # migrations.RunPython(
+        #     load_initial_data
+        # ),
+        #
+        # migrations.AddField(
+        #     model_name='game',
+        #     name='season',
+        #     field=models.ForeignKey(to='nba.Season', default=1),
+        #     preserve_default=False,
+        # ),
         migrations.AddField(
             model_name='season',
             name='season_year',
@@ -29,7 +57,19 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='season',
             name='srid',
-            field=models.CharField(default=None, help_text='the sportsradar global id of the season/schedule', max_length=64, unique=True),
+            field=models.CharField(default='nba', help_text='the sportsradar global id of the season/schedule', max_length=64, unique=True),
+            preserve_default=False,
+        ),
+
+        # add dummy/initial Season
+        migrations.RunPython(
+            load_initial_data
+        ),
+
+        migrations.AddField(
+            model_name='game',
+            name='season',
+            field=models.ForeignKey(to='nba.Season', default=1),
             preserve_default=False,
         ),
     ]
