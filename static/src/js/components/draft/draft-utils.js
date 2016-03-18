7d24e12122737c1ed4f8a9508502c55547cafc2b
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-03-18 14:52:59 -0600

    clean up player search field focus stuff.
    fixes #442

--- static/src/js/components/draft/draft-utils.js
+++ static/src/js/components/draft/draft-utils.js
@@ -7,12 +7,18 @@ import * as AppActions from '../../stores/app-state-store.js';
 
 
 // Clear + focus the player search field. Used when a player is added or removed from the lineup.
-export function focusSearchField() {
-  const searchField = document.querySelectorAll('.cmp-collection-search-filter__input');
+export function focusPlayerSearchField() {
+  // Get the input field, if it is active.
+  const searchField = document.querySelectorAll(
+    '.cmp-collection-search-filter--active .cmp-collection-search-filter__input'
+  );
+  // focus it.
   if (searchField.length) {
-    if (searchField[0].value !== '') {
-      searchField[0].focus();
-      AppActions.clearPlayerSearchField();
-    }
+    searchField[0].focus();
   }
+}
+
+// Clear the value from the player search field
+export function clearPlayerSearchField() {
+  AppActions.clearPlayerSearchField();
 }
