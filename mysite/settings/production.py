e9d93532d6a65d5ad5420ea685b62d106010a96a
cbanister <cbanister@coderden.com>
2016-04-06 15:45:04 -0400

    settings PUSHER_ENABLED in production / dev / local / base

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -70,7 +70,7 @@ INSTALLED_APPS += (
 PUSHER_APP_ID = '144195'
 PUSHER_KEY = '9754d03a7816e43abb64'
 PUSHER_SECRET = environ.get('PUSHER_SECRET')
-PUSHER_ENABLED = environ.get('PUSHER_ENABLED', True)
+PUSHER_ENABLED = 't' in environ.get('PUSHER_ENABLED', 'true') # heroku config vars are strings!
 
 #
 # dataden mongo database connection
