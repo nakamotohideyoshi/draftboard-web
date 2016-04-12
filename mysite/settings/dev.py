5b9b6868384a5d431bca9e5158045156e805ff36
cbanister <cbanister@coderden.com>
2016-04-12 19:18:13 -0400

    MONGO_PORT wants to be an integer

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -81,7 +81,7 @@ MONGO_SERVER_ADDRESS    = environ.get('MONGO_SERVER_ADDRESS')   # ie: '123.132.1
 MONGO_AUTH_DB           = environ.get('MONGO_AUTH_DB')          # 'admin'
 MONGO_USER              = environ.get('MONGO_USER')             # 'admin'
 MONGO_PASSWORD          = environ.get('MONGO_PASSWORD')         # 'dataden1'
-MONGO_PORT              = environ.get('MONGO_PORT')             # 27017
+MONGO_PORT              = int(environ.get('MONGO_PORT'))         # 27017     cast MONGO_PORT to integer!
 MONGO_HOST = environ.get('MONGO_HOST') % ( MONGO_USER,
                                             MONGO_PASSWORD,
                                             MONGO_SERVER_ADDRESS,
