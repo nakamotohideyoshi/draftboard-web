35b89ec1842c111f3a8fba1862acff9cadcbbed8
cbanister <cbanister@coderden.com>
2016-04-13 17:48:18 -0400

    /api/sports/pbp-debug/{game-srid}/{pbp-srid}/   so the front end can check on stuff while were running the FeedTest

---
+++ dataden/serializers.py
@@ -0,0 +1,18 @@
+#
+# dataden/serializers.py
+
+from rest_framework import serializers
+from dataden.models import PbpDebug
+
+class PbpDebugSerializer( serializers.ModelSerializer ):
+
+    class Meta:
+        model   = PbpDebug
+        fields  = (
+            'created',
+            'game_srid',
+            'srid',
+            'timestamp_pushered',
+            'description',
+            'get_delta_seconds',
+        )
