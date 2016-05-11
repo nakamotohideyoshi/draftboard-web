0bf6e4d862e75fca792941d575796f13d9091a58
Zachary Wood <me@zacharywood.com>
2016-05-10 18:50:34 -0600

    fix contest list row test.

--- static/src/js/fixtures/json/store-upcoming-contests.js
+++ static/src/js/fixtures/json/store-upcoming-contests.js
@@ -1,108 +1,204 @@
 module.exports = {
   'allContests': {
-    '863': {
-      'id': 863,
-      'name': '$100 H2H',
-      'sport': 'nba',
+    '1041': {
+      'id': 1041,
+      'name': '$100 MLB 10-Man Tourney',
+      'sport': 'mlb',
       'status': 'scheduled',
-      'start': '2016-03-24T23:00:00Z',
+      'start': '2016-05-11T23:05:00Z',
       'buyin': 100,
-      'draft_group': 152,
-      'max_entries': 1,
-      'prize_structure': 7,
-      'prize_pool': 180,
-      'entries': 2,
-      'current_entries': 0,
-      'gpp': false,
-      'doubleup': false,
-      'respawn': true
+      'draft_group': 268,
+      'max_entries': 3,
+      'prize_structure': {
+        'id': 20,
+        'name': '$100 10 Entry Tournament',
+        'buyin': 100,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 400
+          },
+          {
+            'rank': 2,
+            'value': 300
+          },
+          {
+            'rank': 3,
+            'value': 200
+          }
+        ],
+        'prize_pool': 900
+      },
+      'prize_pool': 900,
+      'entries': 0,
+      'current_entries': 0
     },
-    '919': {
-      'id': 919,
-      'name': '$1 50/50',
+    '1079': {
+      'id': 1079,
+      'name': '$1 NBA 50/50',
       'sport': 'nba',
       'status': 'scheduled',
-      'start': '2016-03-31T23:00:00Z',
+      'start': '2016-05-12T00:00:00Z',
       'buyin': 1,
-      'draft_group': 158,
-      'max_entries': 1,
-      'prize_structure': 39,
-      'prize_pool': 90,
-      'entries': 100,
-      'current_entries': 0,
-      'gpp': true,
-      'doubleup': false,
-      'respawn': false
-    },
-    '920': {
-      'id': 920,
-      'name': '$2 H2H',
-      'sport': 'nhl',
-      'status': 'scheduled',
-      'start': '2016-03-24T23:00:00Z',
-      'buyin': 2,
-      'draft_group': 159,
+      'draft_group': 269,
       'max_entries': 3,
-      'prize_structure': 2,
-      'prize_pool': 3.6,
-      'entries': 2,
-      'current_entries': 0,
-      'gpp': false,
-      'doubleup': false,
-      'respawn': false
+      'prize_structure': {
+        'id': 85,
+        'name': '$1 Fair Match 50/50',
+        'buyin': 1,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 1.8
+          },
+          {
+            'rank': 2,
+            'value': 1.8
+          },
+          {
+            'rank': 3,
+            'value': 1.8
+          },
+          {
+            'rank': 4,
+            'value': 1.8
+          },
+          {
+            'rank': 5,
+            'value': 1.8
+          }
+        ],
+        'prize_pool': 9
+      },
+      'prize_pool': 9,
+      'entries': 0,
+      'current_entries': 0
+    },
+    '1080': {
+      'id': 1080,
+      'name': '$1 NBA H2H',
+      'sport': 'nba',
+      'status': 'scheduled',
+      'start': '2016-05-12T00:00:00Z',
+      'buyin': 1,
+      'draft_group': 269,
+      'max_entries': 3,
+      'prize_structure': {
+        'id': 1,
+        'name': '$1 H2H',
+        'buyin': 1,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 1.8
+          }
+        ],
+        'prize_pool': 1.8
+      },
+      'prize_pool': 1.8,
+      'entries': 0,
+      'current_entries': 0
     }
   },
   'filteredContests': {
-    '863': {
-      'id': 863,
-      'name': '$100 H2H',
-      'sport': 'nba',
+    '1041': {
+      'id': 1041,
+      'name': '$100 MLB 10-Man Tourney',
+      'sport': 'mlb',
       'status': 'scheduled',
-      'start': '2016-03-24T23:00:00Z',
+      'start': '2016-05-11T23:05:00Z',
       'buyin': 100,
-      'draft_group': 152,
-      'max_entries': 1,
-      'prize_structure': 7,
-      'prize_pool': 180,
-      'entries': 2,
-      'current_entries': 0,
-      'gpp': false,
-      'doubleup': false,
-      'respawn': true
+      'draft_group': 268,
+      'max_entries': 3,
+      'prize_structure': {
+        'id': 20,
+        'name': '$100 10 Entry Tournament',
+        'buyin': 100,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 400
+          },
+          {
+            'rank': 2,
+            'value': 300
+          },
+          {
+            'rank': 3,
+            'value': 200
+          }
+        ],
+        'prize_pool': 900
+      },
+      'prize_pool': 900,
+      'entries': 0,
+      'current_entries': 0
     },
-    '919': {
-      'id': 919,
-      'name': '$1 50/50',
+    '1079': {
+      'id': 1079,
+      'name': '$1 NBA 50/50',
       'sport': 'nba',
       'status': 'scheduled',
-      'start': '2016-03-31T23:00:00Z',
+      'start': '2016-05-12T00:00:00Z',
       'buyin': 1,
-      'draft_group': 158,
-      'max_entries': 1,
-      'prize_structure': 39,
-      'prize_pool': 90,
-      'entries': 100,
-      'current_entries': 0,
-      'gpp': true,
-      'doubleup': false,
-      'respawn': false
-    },
-    '920': {
-      'id': 920,
-      'name': '$2 H2H',
-      'sport': 'nhl',
-      'status': 'scheduled',
-      'start': '2016-03-24T23:00:00Z',
-      'buyin': 2,
-      'draft_group': 159,
+      'draft_group': 269,
       'max_entries': 3,
-      'prize_structure': 2,
-      'prize_pool': 3.6,
-      'entries': 2,
-      'current_entries': 0,
-      'gpp': false,
-      'doubleup': false,
-      'respawn': false
+      'prize_structure': {
+        'id': 85,
+        'name': '$1 Fair Match 50/50',
+        'buyin': 1,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 1.8
+          },
+          {
+            'rank': 2,
+            'value': 1.8
+          },
+          {
+            'rank': 3,
+            'value': 1.8
+          },
+          {
+            'rank': 4,
+            'value': 1.8
+          },
+          {
+            'rank': 5,
+            'value': 1.8
+          }
+        ],
+        'prize_pool': 9
+      },
+      'prize_pool': 9,
+      'entries': 0,
+      'current_entries': 0
+    },
+    '1080': {
+      'id': 1080,
+      'name': '$1 NBA H2H',
+      'sport': 'nba',
+      'status': 'scheduled',
+      'start': '2016-05-12T00:00:00Z',
+      'buyin': 1,
+      'draft_group': 269,
+      'max_entries': 3,
+      'prize_structure': {
+        'id': 1,
+        'name': '$1 H2H',
+        'buyin': 1,
+        'ranks': [
+          {
+            'rank': 1,
+            'value': 1.8
+          }
+        ],
+        'prize_pool': 1.8
+      },
+      'prize_pool': 1.8,
+      'entries': 0,
+      'current_entries': 0
     }
   },
   'focusedContestId': null,
