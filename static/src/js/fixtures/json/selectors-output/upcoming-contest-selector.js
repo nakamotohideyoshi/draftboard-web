eb66c512c5890738b072d80aa4b51169eb9719d5
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-06-02 17:54:45 -0600

    Lobby container tests.

---
+++ static/src/js/fixtures/json/selectors-output/upcoming-contest-selector.js
@@ -0,0 +1,108 @@
+module.exports = [
+  {
+    'id': 1522,
+    'name': '$100 MLB 10-Man Tourney',
+    'sport': 'mlb',
+    'status': 'scheduled',
+    'start': '2016-06-02T23:05:00Z',
+    'buyin': 100,
+    'draft_group': 1784,
+    'max_entries': 3,
+    'prize_structure': {
+      'id': 20,
+      'name': '$100 10 Entry Tournament',
+      'buyin': 100,
+      'ranks': [
+        {
+          'rank': 1,
+          'value': 400,
+        },
+        {
+          'rank': 2,
+          'value': 300,
+        },
+        {
+          'rank': 3,
+          'value': 200,
+        },
+      ],
+      'prize_pool': 900,
+      'is_h2h': false,
+    },
+    'prize_pool': 900,
+    'entries': 0,
+    'current_entries': 0,
+    'contest_size': 10,
+    'entryInfo': [
+
+    ],
+  },
+  {
+    'id': 1523,
+    'name': '$100 MLB H2H',
+    'sport': 'mlb',
+    'status': 'scheduled',
+    'start': '2016-06-02T23:05:00Z',
+    'buyin': 100,
+    'draft_group': 1784,
+    'max_entries': 3,
+    'prize_structure': {
+      'id': 7,
+      'name': '$100 H2H',
+      'buyin': 100,
+      'ranks': [
+        {
+          'rank': 1,
+          'value': 180,
+        },
+      ],
+      'prize_pool': 180,
+      'is_h2h': true,
+    },
+    'prize_pool': 180,
+    'entries': 0,
+    'current_entries': 0,
+    'contest_size': 2,
+    'entryInfo': [
+
+    ],
+  },
+  {
+    'id': 1524,
+    'name': '$50 MLB 10-Man Tourney',
+    'sport': 'mlb',
+    'status': 'scheduled',
+    'start': '2016-06-02T23:05:00Z',
+    'buyin': 50,
+    'draft_group': 1784,
+    'max_entries': 3,
+    'prize_structure': {
+      'id': 65,
+      'name': '$50 10-Entry (3 paid)',
+      'buyin': 50,
+      'ranks': [
+        {
+          'rank': 1,
+          'value': 200,
+        },
+        {
+          'rank': 2,
+          'value': 150,
+        },
+        {
+          'rank': 3,
+          'value': 100,
+        },
+      ],
+      'prize_pool': 450,
+      'is_h2h': false,
+    },
+    'prize_pool': 450,
+    'entries': 0,
+    'current_entries': 0,
+    'contest_size': 10,
+    'entryInfo': [
+
+    ],
+  },
+];
