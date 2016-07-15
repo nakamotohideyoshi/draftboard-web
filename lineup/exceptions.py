55abc568488d912396ae34c2cd827a23cfd03cb8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-07-15 13:17:48 -0600

    yank common lineup lib stuff into it's own module.
    pep8 format exceptions file.

--- lineup/exceptions.py
+++ lineup/exceptions.py
@@ -1,62 +1,85 @@
 #
 # lineup/exceptions.py
 
+
 class LineupDoesNotMatchUser(Exception):
+
     def __init__(self):
         super().__init__(
-           "The lineup does not match the User's account" )
+            "The lineup does not match the User's account")
 
-class LineupDoesNotMatchExistingEntryLineup(Exception): pass
+
+class LineupDoesNotMatchExistingEntryLineup(Exception):
+    pass
     # def __init__(self, lineup_name):
-    #     super().__init__("Lineup must match the existing lineup '%s' for this Contest." % lineup_name)
+    # super().__init__("Lineup must match the existing lineup '%s' for this Contest." % lineup_name)
+
 
 class LineupInvalidRosterSpotException(Exception):
+
     def __init__(self):
         super().__init__(
-           "The player selected cannot be drafted for the roster spot" )
+            "The player selected cannot be drafted for the roster spot")
+
 
 class InvalidLineupSizeException(Exception):
+
     def __init__(self):
         super().__init__(
-           "You have not drafted enough players to make a lineup" )
+            "You have not drafted enough players to make a lineup")
+
 
 class PlayerDoesNotExistInDraftGroupException(Exception):
+
     def __init__(self, player_id, draftgroup_id):
         super().__init__(
-           "The player ["+str(player_id)+"] does not exist in the draftgroup #"+str(draftgroup_id))
+            "The player ["+str(player_id)+"] does not exist in the draftgroup #"+str(draftgroup_id))
+
 
 class InvalidLineupSalaryException(Exception):
+
     def __init__(self, username, salary, max_team_salary):
         super().__init__(
-           "The User["+username+"]'s salary for the lineup was:"+str(salary)+
-           " but it was expected to be less than:"+str(max_team_salary))
+            "Lineup is over the maximum salary of $%s." % str(max_team_salary))
+
 
 class DuplicatePlayerException(Exception):
+
     def __init__(self):
         super().__init__(
-           "You have a lineup with duplicate players" )
+            "You have a lineup with duplicate players")
+
 
 class PlayerSwapGameStartedException(Exception):
+
     def __init__(self):
         super().__init__(
-           "You cannot swap players that are already in games" )
+            "You cannot swap players that are already in games")
+
 
 class EditLineupInProgressException(Exception):
+
     def __init__(self):
         super().__init__(
-           "You are currently saving an edit. Please try again later.")
+            "You are currently saving an edit. Please try again later.")
+
 
 class LineupUnchangedException(Exception):
+
     def __init__(self):
         super().__init__(
-           "The lineup you are trying to edit had not changes.")
+            "The lineup you are trying to edit had not changes.")
+
 
 class CreateLineupExpiredDraftgroupException(Exception):
+
     def __init__(self):
         super().__init__(
-           "The draftgroup has expired, you cannot create a new lineup for this contest.")
+            "The draftgroup has expired, you cannot create a new lineup for this contest.")
+
 
 class NotEnoughTeamsException(Exception):
+
     def __init__(self):
         super().__init__(
             "Lineup must consist of players from at least three different teams.")
