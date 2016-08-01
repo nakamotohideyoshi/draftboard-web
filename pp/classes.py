3872db48097ba08d8ede53125168a8e57a59457e
cbanister <cbanister@coderden.com>
2016-08-01 18:07:13 -0400

    Continuing work on vzero paypal

--- pp/classes.py
+++ pp/classes.py
@@ -1,21 +1,236 @@
 #
 # classes.py
 
-from mysite import settings
+from django.conf import settings
 import requests
 import random
 import json
 from util.timesince import timeit
 import paypalrestsdk as paypal
 from rest_framework.exceptions import APIException
+import pp.serializers
 from pp.models import (
     SavedCardPaymentData,
     CreditCardPaymentData,
     PayPalAccountPaymentData,
 )
+import braintree
 
-# import logging
-# logging.basicConfig(level=logging.INFO)
+class VZeroShipping(object):
+
+    class ValidationError(Exception): pass
+
+    field_first_name            = "first_name"
+    field_last_name             = "last_name"
+    #field_company               = "company"
+    field_street_address        = "street_address"
+    field_extended_address      = "extended_address"
+    field_locality              = "locality"            # for US: this is the city, ie: "Austin"
+    field_region                = "region"              # for US: this is the state code, ie: "TX"
+    field_postal_code           = "postal_code"         # for US: this is the zipcode, ie: "12345"
+    field_country_code_alpha2   = "country_code_alpha2"
+
+    valid_fields = [
+        field_first_name,
+        field_last_name,
+        #field_company,
+        field_street_address,
+        field_extended_address,
+        field_locality,
+        field_region,
+        field_postal_code,
+        field_country_code_alpha2,
+    ]
+
+    def __init__(self, data=None):
+        self.data = data
+        if self.data is None:
+            self.data = {
+                "first_name":           None,           # "Jen",
+                "last_name":            None,           # "Smith",
+                #"company":              "",             # "ABC Co.",
+                "street_address":       None,           # "1 E 1st St",
+                "extended_address":     "",             # "Suite 403",
+                "locality":             None,           # "Bartlett",
+                "region":               None,           # "IL",
+                "postal_code":          None,           # "60103",
+                "country_code_alpha2":  None,           # "US"
+            }
+
+    def validate(self):
+        """
+        validates the internal data
+
+        raises any validation exceptions if they exist.
+        """
+        for field in self.valid_fields:
+            if self.data.get(field) is None:
+                raise self.ValidationError("'%s' must be set." % field)
+
+    def update_field(self, field, val):
+        if not isinstance(val, str):
+            raise Exception('param "val" should be a string!')
+        self.data[field] = val
+
+    # def update_shipping_information(self, first_name, last_name, company,
+    #                                         street_address, extended_address,
+    #                                         locality, region, postal_code, country_code_alpha2):
+    #     """ update the shipping address by setting each field manually """
+    #     pass
+
+    def update_data(self, data):
+        self.data = data
+        self.validate()
+
+    def from_serializer(self, vzero_shipping_serializer):
+        vzero_shipping_serializer.is_valid(raise_exception=True)
+        self.update_data(vzero_shipping_serializer.data)
+
+class VZeroTransaction(object):
+
+    class ValidationError(Exception): pass
+
+    default_customer_cc_statement_description = "Draftboard"
+    default_paypal_email_receipt = "draftboard.com deposit"
+
+    default_currency = "USD"
+    choices_currency = [default_currency] # we may add CAD (canadien) eventually...
+
+    field_amount                = "amount"
+    field_currency              = "merchant_account_id"     # strange name, but here we set the currency
+    field_payment_method_nonce  = "payment_method_nonce"
+    field_shipping              = "shipping"
+
+    valid_fields = [
+        field_amount,
+        field_currency,
+        field_payment_method_nonce
+
+        # purposely excludes 'shipping' because its a dict and should validate itself
+    ]
+
+    def __init__(self):
+        # construct the data with some None values internally.
+        # we will perform validation on it before being used.
+        self.data = {
+
+            "amount" : None,                                        # TODO - this will need to be set
+            "merchant_account_id": self.default_currency,           # defaults to "USD"
+            "payment_method_nonce" : None,                          # TODO - will need to set
+            "order_id" : "Mapped to PayPal Invoice Number",
+            "descriptor": {
+                #
+                # Descriptor displayed in customer CC statements. [22 char max]
+                "name" : self.default_customer_cc_statement_description
+            },
+
+            "shipping": None,                                       # TODO - validate its been set
+
+            "options" : {
+                "paypal" : {
+                    "custom_field" : "PayPal custom field",
+                    #
+                    # "Description for PayPal email receipt"
+                    "description" : self.default_paypal_email_receipt,
+              },
+            }
+        }
+
+    def validate(self):
+        """
+        raises an exception if a required field has not been set.
+        """
+        for field in self.valid_fields:
+            if self.data.get(field) is None:
+                raise self.ValidationError("'%s' field must be set." % field)
+
+    def update_field(self, field, val):
+        self.data[field] = val
+
+    def update_shipping_data(self, data):
+        # update the shipping data
+        self.update_field(self.field_shipping, data)
+
+    def update_transaction_data(self, data):
+        self.update_field(self.field_amount, data.get(self.field_amount))
+        self.update_field(self.field_payment_method_nonce, data.get(self.field_payment_method_nonce))
+
+    def update_data(self, shipping_data, transaction_data):
+        """
+
+        """
+
+        # update the shipping data
+        self.update_shipping_data(shipping_data)
+
+        # update the transaction data (amount, currency, nonce...)
+        self.update_transaction_data(transaction_data)
+
+        # raise exceptions if any required fields are not set
+        self.validate()
+
+class VZero(object):
+
+    class VZeroException(Exception): pass
+
+    access_token = settings.VZERO_ACCESS_TOKEN
+
+    def __init__(self, access_token=None): # add user param ?
+        # django user who is performing a transaction
+        #self.user = user
+
+        # override the access_token if its specified
+        if access_token is not None:
+            self.access_token = access_token
+
+        # setup the gateway which will help us do braintree/vzero things
+        self.gateway = braintree.BraintreeGateway(access_token=self.access_token)
+
+    def get_client_token(self):
+        """
+        the client requires our server to provide this token so they
+        can do client stuff and build a payment_method_nonce which
+        will be given back to the server at the point they wish
+        to make a transaction.
+
+        :return: client_token
+        """
+        client_token = self.gateway.client_token.generate()
+        return client_token
+
+    # test this if we are going to use it
+    # def create_transaction_from_serializer(self, vzero_deposit_serializer):
+    #     vzero_transaction = VZeroTransaction()
+    #     vzero_transaction.from_serializer(vzero_deposit_serializer)
+    #
+    #     # create the transaction sale, and return the result
+    #     return self.create_transaction(vzero_transaction)
+
+    def create_transaction(self, vzero_transaction):
+        """
+        create the transaction using the vzero serializer for easy use
+        """
+
+        # use the gateway to make the sale (aka: the deposit)
+        result = self.gateway.transaction.sale(
+            vzero_transaction.data
+        )
+
+        #
+        if not result.is_success:
+            #
+            # example of deep errors (a list)
+            #   [<ValidationError {attribute: 'payment_method_nonce', message: 'Unknown or expired payment_method_nonce.', code: '91565'} at 140660839155080>]
+            # TODO remove this debugging print eventually !
+            print("braintree transaction | Failure deep errors: %s" % str(result.errors.deep_errors))
+            for e in result.errors.deep_errors:
+                raise self.VZeroException(e.message)
+
+        #
+        # success
+        # TODO remove this debugging print eventually !
+        print("braintree transaction | Success ID: %s" % str(result.transaction.id))
+        return result.transaction.id
 
 # these 4 un-classed methods are for testing. they come from wikipedia
 #    source: https://en.wikipedia.org/wiki/Luhn_algorithm
@@ -467,234 +682,3 @@ class PayPal(object):
 
         self.r_list_cards = self.session.get(url, headers=self.get_headers())
         return self.get_http_response_dict(self.session, self.r_list_cards)
-
-# #
-# ##########################################################################################
-# # old code
-# ##########################################################################################
-# class Payout( object ):
-#     #
-#     #
-#     WITHDRAW_STATUS_PROCESSED = cash.withdraw.constants.WithdrawStatusConstants.Processed.value
-#
-#     IN_PROGRESS_STATUSES = [
-#         'PROCESSING',
-#         'SUCCESS'
-#     ]
-#
-#     PROCESSED_STATUSES = [
-#         'SUCCESS'  # notably not 'PROCESSED'
-#     ]
-#
-#     api = 'https://api.sandbox.paypal.com' # 'https://api.paypal.com'
-#
-#     api_oauth_token = api + '/v1/oauth2/token'
-#     api_payout      = api + '/v1/payments/payouts'
-#
-#     def __init__(self, model_instance):
-#         self.STATUS_SUCCESS = cash.withdraw.models.WithdrawStatus.objects.get(pk=self.WITHDRAW_STATUS_PROCESSED)
-#
-#         self.model_instance = model_instance
-#
-#         self.client_id  = 'ATqLK_YEFwhEGOF_28TQLxXq-MG88suXKQZm0k4UjrfkrvXwxSRbv6mgPO8moTdLHeJ3zFb-t8sBdKLg'
-#         self.secret     = 'EFuyMSsA8EcOabrJBqPvbUW-0ZnRJ8ym8XrxPDje9GeTuJOF-Dxcn4gLI9hNR79chUE_MO4Y_u6mMQrQ'
-#
-#         self.session = None
-#
-#         self.r_login = None
-#         self.r_payout = None
-#
-#         self.sender_batch_id = None
-#         self.payout_batch_id = None
-#
-#     def auth(self):
-#         headers = {
-#             'Accept' : 'application/json',
-#             'Accept-Language' : 'en_US'
-#         }
-#         post_data = {
-#             'grant_type' : 'client_credentials'
-#         }
-#         self.session = requests.Session()
-#         self.r_login = self.session.post( self.api_oauth_token,
-#                                           headers=headers, data=post_data,
-#                                           auth=(self.client_id, self.secret))
-#         print( self.r_login.status_code )
-#         print( self.api_oauth_token )
-#         print( self.r_login.text )
-#
-#         self.model_instance.auth_status = str(self.r_login.status_code)
-#         self.model_instance.save()
-#         return json.loads( self.r_login.text )
-#
-#     def payout_async(self, withdraws=[]):
-#         """
-#         call the payout() method using a celery task to perform asynchronous payout
-#
-#         :param withdraws:
-#         :return:
-#         """
-#         pass
-#
-#     def payout(self, get_until_processed=True):
-#         """
-#         this method takes very tangible time - usually like 15-30 seconds.
-#
-#         if 'get_until_processed' is set to False, this will return immediately,
-#         once the payout has been submitted, but paypal will still take some time
-#         to full process the payout so you will likely not know at this point
-#         if the payout has succeeded or failed. use get() to check on the status
-#
-#         :param to_email:
-#         :param amount:
-#         :return:
-#         """
-#
-#         #
-#         # login
-#         if self.session is None:
-#             auth_response = self.auth()
-#
-#         if self.model_instance.paypal_transaction:
-#             # if it exists, we need to check it we ever paid this transaction out !
-#             check_get_json = self.get( batch_id=self.model_instance.paypal_transaction, save=False )
-#             batch_status = check_get_json.get('batch_header').get('batch_status')
-#             if batch_status in self.IN_PROGRESS_STATUSES:
-#                 tid = self.model_instance.paypal_transaction
-#                 msg = 'transaction has already been processed! paypal_transaction: ' + str(tid)
-#                 print( msg )
-#                 #raise Exception( msg )
-#                 return
-#
-#         #
-#         # issue the payout
-#         j = json.loads( self.r_login.text )
-#         headers = {
-#             'Content-Type'  : 'application/json',
-#             'Authorization' : '%s %s' % (j.get('token_type'), j.get('access_token'))
-#         }
-#         self.sender_batch_id = ''.join(random.choice(string.ascii_uppercase) for i in range(12))
-#         post_data = {
-#             "sender_batch_header" : {
-#                 "sender_batch_id"   : "%s" % self.sender_batch_id,
-#                 "email_subject"     : "You have a Payout!",
-#                 "recipient_type"    : "EMAIL"
-#             },
-#             "items" : [
-#                 {
-#                     "recipient_type" : "EMAIL",
-#                     "amount" : {
-#                         "value"     : "%s" % str( float( abs( self.model_instance.amount ) ) ),
-#                         "currency"  : "USD"
-#                     },
-#                     "note"              : "Thanks for your patronage!",
-#                     "sender_item_id"    : "201403140001",
-#                     "receiver"          : "%s" % self.model_instance.email
-#                 }
-#             ]
-#         }
-#         #j = json.loads( json.dumps( post_data ) )
-#         self.r_payout = self.session.post( self.api_payout,
-#                                            headers=headers,
-#                                            data=json.dumps( post_data ) )
-#         print( self.r_payout.status_code )
-#         print( 'POST', self.api_payout )
-#         print( self.r_payout.text )
-#         j = json.loads( self.r_payout.text )
-#
-#         #
-#         # this error is possible:
-#         # {"name":"VALIDATION_ERROR","message":"Invalid request - see details.",
-#         #   "debug_id":"943be7f1a0f95",
-#         #   "information_link":"https://developer.paypal.com/webapps/developer/docs/api/#VALIDATION_ERROR",
-#         #   "details":[{"field":"items[0].receiver","issue":"Required field missing"}]}
-#         try:
-#             self.payout_batch_id = j.get('batch_header').get('payout_batch_id')
-#         except AttributeError: # ie: 'batch_header' didnt exist
-#             # stash error in paypal model withdraw
-#             self.model_instance.paypal_errors = self.r_payout.text
-#             self.model_instance.save()
-#             print( self.r_payout.text )
-#             print( 'payout failed! check the admin page for withdraws for error messages from paypal')
-#             return
-#
-#         #
-#         # set the payal transaction id in the model, along with the current status
-#         self.paypal_transaction = self.payout_batch_id
-#         self.model_instance.paypal_transaction = self.paypal_transaction
-#         self.model_instance.payout_status = j.get('batch_header').get('batch_status')
-#         self.model_instance.save()
-#
-#         if get_until_processed:
-#             #
-#             # call get() until we've updated to the PROCESSED status
-#             while True: # scary - but we will make sure to add very long timeout to the calling task
-#                 time.sleep( 5.0 )
-#                 get_json = self.get()
-#                 payout_status = get_json.get('batch_header').get('batch_status')
-#                 print( 'payout GET status:', payout_status )
-#                 if payout_status in self.PROCESSED_STATUSES: # ie: failure may be in here!
-#                     #
-#                     # update the master success in the admin, plus any papal error
-#                     self.model_instance.status = self.STATUS_SUCCESS
-#                     self.model_instance.save()
-#                     break
-#
-#         return j
-#
-#     def get(self, batch_id=None, save=True):
-#         if batch_id is None:
-#             batch_id = self.payout_batch_id
-#         print( 'get', batch_id )
-#
-#         j = json.loads( self.r_login.text )
-#         headers = {
-#             'Authorization' : '%s %s' % (j.get('token_type'), j.get('access_token'))
-#         }
-#         self.r_get = self.session.get( self.api_payout + '/' + str(batch_id), headers=headers )
-#         print( self.r_get.status_code )
-#         print( 'GET', self.api_payout )
-#         print( self.r_get.text )
-#
-#         j = json.loads( self.r_get.text )
-#
-#         if save:
-#             self.model_instance.get_status = j.get('batch_header').get('batch_status')
-#             self.model_instance.save()
-#
-#         return j
-#
-#         # response to payout_batch_id (the master of all of the items)
-#         # {'batch_header': {'fees': {'value': '0.0', 'currency': 'USD'}, 'amount': {'value': '0.01', 'currency': 'USD'}, 'time_completed': '2015-04-22T04:17:34Z', 'time_created': '2015-04-22T04:17:09Z', 'payout_batch_id': 'AP393JT3TEUF2', 'batch_status': 'SUCCESS', 'sender_batch_header': {'sender_batch_id': 'FUFFEPGMVBGX', 'email_subject': 'You have a Payout!'}}, 'items': [{'links': [{'method': 'GET', 'href': 'https://api.sandbox.paypal.com/v1/payments/payouts-item/HVBG5X7F9MYSS', 'rel': 'item'}], 'payout_item': {'recipient_type': 'EMAIL', 'receiver': 'testtest@coderden.com', 'sender_item_id': '201403140001', 'note': 'Thanks for your patronage!', 'amount': {'value': '0.01', 'currency': 'USD'}}, 'transaction_id': '68696867XP9554523', 'transaction_status': 'SUCCESS', 'payout_item_fee': {'value': '0.0', 'currency': 'USD'}, 'time_processed': '2015-04-22T04:17:26Z', 'payout_batch_id': 'AP393JT3TEUF2', 'payout_item_id': 'HVBG5X7F9MYSS'}], 'links': [{'method': 'GET', 'href': 'https://api.sandbox.paypal.com/v1/payments/payouts/AP393JT3TEUF2', 'rel': 'self'}]}
-#
-#     def payout_debug_test_error_50_percent(self):
-#         """
-#         delays for a few seconds, and then, randomly decides to succeed or error.
-#
-#         returns a randomly generated string of 12 characters upon success
-#
-#         :return:
-#         """
-#         r = random.Random()
-#         status = bool( r.randrange(0, 2) )     # randomly a 0 or a 1
-#
-#         random_transaction_id = ''.join(random.choice(string.ascii_uppercase) for i in range(12))
-#         self.debug_delay_sec_payout( status=status )
-#
-#     def debug_delay_sec_payout(self, t=5.0, status=False):
-#         time.sleep( t )
-#
-#         if status == False:
-#             raise Exception('debug_delay_sec_payout - randomized exception')
-#
-#         print( 'the task [debug_delay_sec_payout] had this for status:', str(status) )
-#
-# def test_app():
-#     heartbeat.delay()
-#
-# def test_payout(pk=1):
-#     #p = Payout( to_email='testtest@coderden.com', amount=0.10 )
-#     #return payout.delay( instance=p )
-#     ppw = cash.withdraw.models.PayPalWithdraw.objects.get( pk = pk )
-#     p = Payout( model_instance=ppw )
-#     return payout.apply_async( (p, ), serializer='pickle' )
