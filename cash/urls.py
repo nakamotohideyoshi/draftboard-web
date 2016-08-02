1e0936a25f06cac16f72ca9bf233aa9fcce6d3f4
cbanister <cbanister@coderden.com>
2016-08-02 18:01:09 -0400

    updates for nfl replay building, nfl parsing, nfl trigger, and cleaned up a bunch of the django110 warnings for a bunch of the apps urls

--- cash/urls.py
+++ cash/urls.py
@@ -1,19 +1,20 @@
-from django.conf.urls import url
+#
+# urls.py
 
+from django.conf.urls import url
 from cash.views import TransactionHistoryAPIView, BalanceAPIView
-# from cash.views import DepositView
 from cash.withdraw.views import CheckWithdrawAPIView
 
-
 urlpatterns = [
-    url(r'^transactions/(?P<user_id>[0-9]+)/$', TransactionHistoryAPIView.as_view()),
-    url(r'^transactions/$', TransactionHistoryAPIView.as_view()),
-    url(r'^balance/$', BalanceAPIView.as_view()),
 
-    # this was the paypal deposit url
-    # (r'^deposit/$', DepositView.as_view()),
+    url(r'^transactions/(?P<user_id>[0-9]+)/$', TransactionHistoryAPIView.as_view()),
+
+    url(r'^transactions/$', TransactionHistoryAPIView.as_view()),
+
+    url(r'^balance/$', BalanceAPIView.as_view()),
 
     #
     # user can request a withdraw of their funds via check
     url(r'^withdraw/check/$', CheckWithdrawAPIView.as_view()),
+
 ]
