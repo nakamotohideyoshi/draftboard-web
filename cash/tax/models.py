5e7d45b27449c177f16c4db9082e2600d9a7bdc7
cbanister <cbanister@coderden.com>
2016-08-05 20:13:55 -0400

    paypal withdraw

--- cash/tax/models.py
+++ cash/tax/models.py
@@ -13,4 +13,18 @@ class Tax( models.Model ):
     class Meta:
         unique_together = ('tax_identifier', 'user')
 
+class TaxForm1099(models.Model):
+    """
+    a token for whether the tax form/link was sent to the user
+    """
 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, null=False)
+    year = models.IntegerField(null=False)
+    sent = models.BooleanField(null=False, default=False)
+    net_profit = models.FloatField(null=False, default=0.0)
+
+    class Meta:
+        unique_together = ('user', 'year')
--- cash/withdraw/tasks.py
+++ cash/withdraw/tasks.py
@@ -1,2 +1,55 @@
+from __future__ import absolute_import
+
 #
 # tasks.py
+
+from django.conf import settings
+from django.core.cache import cache
+from mysite.celery_app import app
+from datetime import timedelta
+from django.utils import timezone
+from django.core.mail import send_mail
+from cash.withdraw.models import PayPalWithdraw
+
+#
+FROM_EMAIL = 'admin@draftboard.com'
+
+#
+# emails to receive Withdraw request emails
+EMAILS = [
+    'caleb@coderden.com',
+    'manager@draftboard.com',
+    'pedro@draftboard.com',
+]
+
+LOCK_EXPIRE         = 60  # lock expires in this many seconds
+SHARED_LOCK_NAME    = 'withdraw_request_emails'
+
+LOCK_EXPIRE         = 60  # lock expires in this many seconds
+SHARED_LOCK_NAME    = 'spawn_contest_pool_contests'
+
+@app.task(bind=True)
+def notify_recent_withdraws(self):
+    """
+    email the neccessary people with information about recent withdraws
+    """
+    lock_id = 'task-LOCK--%s--%s' % ('all_sports', SHARED_LOCK_NAME)
+
+    acquire_lock = lambda: cache.add(lock_id, 'lock', LOCK_EXPIRE)
+    release_lock = lambda: cache.delete(lock_id)
+
+    withdraws = PayPalWithdraw.objects.filter(status__category__in=['Pending'])
+    if withdraws.count() == 0:
+        return # not notifications required if there arent any
+
+    if acquire_lock():
+        try:
+            #
+
+            subject = 'Draftboard has %s Pending Withdraws.' % (withdraws.count())
+            body = 'heres the link: %s/admin/withdraw/paypalwithdraw/' % settings.DOMAIN
+
+            send_mail(subject, body, FROM_EMAIL, EMAILS)
+
+        finally:
+            release_lock()
