2e6d3ade0505f9cc8d8a2e017c6ad031421334fa
Craig Ruks <craig@craigruks.com>
2016-08-08 15:16:50 +0700

    Fixing rebase

--- lobby/urls.py
+++ lobby/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls import url
 from lobby.views import ContestBannerAPIView
 
 urlpatterns = [
-urlpatterns = [
 
     #
     # get all the contest banners
     url(r'^featured-content/$', ContestBannerAPIView.as_view()),
-
 ]
