89f261e4af31109e6c7890375d04b6dd4c3cde2d
Craig Ruks <craig@craigruks.com>
2016-08-25 16:37:28 +0700

    Latest import of Justen's code from draftboard-animations repo, now linted!

--- static/src/js/lib/live-animations/nfl/NFLPlayRecapVO.js
+++ static/src/js/lib/live-animations/nfl/NFLPlayRecapVO.js
@@ -1,236 +1,225 @@
 /**
- *  Value object representing a play recap.
- */
- export default class NFLPlayRecapVO {
+*  Value object representing a play recap.
+*/
+export default class NFLPlayRecapVO {
 
-    constructor (data) {
-        this._obj = data;
+  constructor(data) {
+    this._obj = data;
+  }
+
+  static get LEFT_TO_RIGHT() {
+    return 'leftToRight';
+  }
+
+  static get RIGHT_TO_LEFT() {
+    return 'rightToLeft';
+  }
+
+  static get MIDDLE() {
+    return 'middle';
+  }
+
+  static get RIGHT() {
+    return 'right';
+  }
+
+  static get LEFT() {
+    return 'left';
+  }
+
+  static get PASS() {
+    return 'pass';
+  }
+
+  static get RUSH() {
+    return 'rush';
+  }
+
+  static get SCAMBLE() {
+    return 'scramble';
+  }
+
+  static get HANDOFF() {
+    return 'handoff';
+  }
+
+  static get PASS_DEEP() {
+    return 'pass_deep';
+  }
+
+  /**
+   * The starting yard line of the play.
+   */
+  startingYardLine() {
+    return this._obj.yardlineStart;
+  }
+
+  /**
+   * The ending yard line of the play.
+   */
+  endingYardLine() {
+    return this._obj.yardlineEnd;
+  }
+
+  /**
+   * The total yards of the play.
+   */
+  totalYards() {
+    return Math.abs(this.endingYardLine() - this.startingYardLine());
+  }
+
+  /**
+   * ...
+   */
+  whichSide() {
+    return this._obj.whichSide;
+  }
+
+  /**
+   * The type of play.
+   * @return {string}
+   */
+  playType() {
+    return this._obj.type;
+  }
+
+  /**
+   * The play formation before the snap.
+   * @return {string}
+   */
+  playFormation() {
+    return this._obj.formation;
+  }
+
+  /**
+   * String representing the Quarterbacks action.
+   * @return {string|null}
+   */
+  qbAction() {
+    if (this.isScramble()) {
+      return NFLPlayRecapVO.SCAMBLE;
+    } else if (this.isHandOff()) {
+      return NFLPlayRecapVO.HANDOFF;
+    } else if (this.isPassingPlay()) {
+      return this.passType();
+    }
+    return null;
+  }
+
+  /**
+   * The type of pass being thrown by quarterback.
+   * @return {string}
+   */
+  passType() {
+    if (!this.isPassingPlay()) {
+      return null;
+    } else if (this.passDistance() > 0.2) {
+      return NFLPlayRecapVO.PASS_DEEP;
+    }
+    return NFLPlayRecapVO.PASS;
+  }
+
+  /**
+   * The distance of the pass in yards.
+   * @return {number}
+   */
+  passDistance() {
+    if (!this.isPassingPlay()) {
+      return 0;
     }
 
-    static get LEFT_TO_RIGHT () {
-        return 'leftToRight';
+    if (this.isTurnover()) {
+      // Return the distance the defensmen would have intercepted
+      // the ball at, before running backwards.
+      return Math.abs(this.totalYards() + this.rushDistance());
     }
 
-    static get RIGHT_TO_LEFT () {
-        return 'rightToLeft';
+    // The passing distance is the forward progress minus any
+    // additional rushing distance.
+    return Math.abs(this.totalYards() - this.rushDistance());
+  }
+
+  /**
+   * The distance the ball was carried. For passing plays this will
+   * be the distance the ball was carried after the catch.
+   * @return {number}
+   */
+  rushDistance() {
+    if (this.isPassingPlay()) {
+      // Convert the yardsAfterCatch to a float that matches the
+      // format of the yardlineStart property. This makes
+      // it easy to determine where a catch occurs by subtracting
+      // the yardlineEnd() by the rushDistance().
+      return this._obj.pass.yardsAfterCatch / 100;
+    } else if (this.isRushingPlay()) {
+      return this.totalYards();
     }
+    return 0;
+  }
 
-    static get MIDDLE () {
-        return 'middle';
+  /**
+   * The side of the field the play finishes on. Possible values are
+   * "left", "middle", "right". defaulting to "middle"
+   * @return {string}
+   */
+  side() {
+    if (this.isPassingPlay()) {
+      return this._obj.pass.side;
+    } else if (this.isRushingPlay()) {
+      return this._obj.rush.side;
     }
+    return NFLPlayRecapVO.MIDDLE;
+  }
 
-    static get RIGHT () {
-        return 'right'
+  /**
+   * The plays drive direction. Possible values are "rightToLeft"
+   * or "leftToRight".
+   * @return {string}
+   */
+  driveDirection() {
+    if (this._obj.driveDirection === NFLPlayRecapVO.LEFT_TO_RIGHT) {
+      return NFLPlayRecapVO.LEFT_TO_RIGHT;
     }
+    return NFLPlayRecapVO.RIGHT_TO_LEFT;
+  }
 
-    static get LEFT () {
-        return 'left'
-    }
+  /**
+   * Returns true if the QB hands off the ball.
+   * @return {boolean}
+   */
+  isHandOff() {
+    return !this.isPassingPlay() && !this.isScramble();
+  }
 
-    static get PASS () {
-        return 'pass'
-    }
+  /**
+   * Returns true if the QB sneeks the ball.
+   * @return {boolean}
+   */
+  isScramble() {
+    return this.isRushingPlay() && this._obj.rush.scramble;
+  }
 
-    static get RUSH () {
-        return 'rush'
-    }
+  /**
+   * Returns true if the pass results in an interception or fumble.
+   * @return {boolean}
+   */
+  isTurnover() {
+    return this.isPassingPlay() && this._obj.pass.intercepted;
+  }
 
-    static get SCAMBLE () {
-        return 'scramble'
-    }
+  /**
+   * Returns true if the QB passes the ball.
+   * @return {boolean}
+   */
+  isPassingPlay() {
+    return this.playType() === NFLPlayRecapVO.PASS;
+  }
 
-    static get HANDOFF () {
-        return 'handoff'
-    }
-
-    static get PASS_DEEP () {
-        return 'pass_deep'
-    }
-
-
-    /**
-     * The starting yard line of the play.
-     */
-    startingYardLine () {
-        return this._obj.yardlineStart;
-    }
-
-    /**
-     * The ending yard line of the play.
-     */
-    endingYardLine () {
-        return this._obj.yardlineEnd;
-    }
-
-    /**
-     * The total yards of the play.
-     */
-     totalYards () {
-        return Math.abs(this.endingYardLine() - this.startingYardLine());
-     }
-
-    /**
-     * ...
-     */
-    whichSide () {
-        return this._obj.whichSide;
-    }
-
-    /**
-     * The type of play.
-     * @return {string} 
-     */
-    playType () {
-        return this._obj.type;
-    }
-
-    /**
-     * The play formation before the snap.
-     * @return {string}
-     */
-    playFormation () {
-        return this._obj.formation;
-    }
-
-    /**
-     * String representing the Quarterbacks action.
-     * @return {string|null}
-     */
-    qbAction () {
-        if (this.isScramble()) {
-            return NFLPlayRecapVO.SCAMBLE;
-        } else if (this.isHandOff()) {
-            return NFLPlayRecapVO.HANDOFF;
-        } else if (this.isPassingPlay()) {
-            return this.passType();
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * The type of pass being thrown by quarterback.
-     * @return {string}
-     */
-    passType () {
-        if (!this.isPassingPlay()) {
-            return null;
-        } else if (this.passDistance() > .2) {
-            return NFLPlayRecapVO.PASS_DEEP;
-        } else {
-            return NFLPlayRecapVO.PASS;
-        }
-    }
-
-    /**
-     * The distance of the pass in yards.
-     * @return {number}
-     */
-    passDistance () {
-        if (!this.isPassingPlay()) {
-            return 0;
-        }
-
-        if (this.isTurnover()) {
-            // Return the distance the defensmen would have intercepted
-            // the ball at, before running backwards.
-            return Math.abs(this.totalYards() + this.rushDistance());
-        }
-        
-        // The passing distance is the forward progress minus any
-        // additional rushing distance.
-        return Math.abs(this.totalYards() - this.rushDistance());
-    }
-
-    /**
-     * The distance the ball was carried. For passing plays this will
-     * be the distance the ball was carried after the catch.
-     * @return {number}
-     */
-    rushDistance () {
-        if (this.isPassingPlay()) {
-            
-            // Convert the yardsAfterCatch to a float that matches the
-            // format of the yardlineStart property. This makes
-            // it easy to determine where a catch occurs by subtracting
-            // the yardlineEnd() by the rushDistance().
-            return this._obj.pass.yardsAfterCatch / 100;
-
-        } else if (this.isRushingPlay()) {
-
-            return this.totalYards(); 
-        
-        } else {
-        
-            return 0;
-        }
-    }
-
-    /**
-     * The side of the field the play finishes on. Possible values are
-     * "left", "middle", "right". defaulting to "middle"
-     * @return {string}
-     */
-    side () {
-        if (this.isPassingPlay()) {
-            return this._obj.pass.side;
-        } else if (this.isRushingPlay()) {
-            return this._obj.rush.side;
-        } else {
-            return NFLPlayRecapVO.MIDDLE;
-        }
-    }
-
-    /**
-     * The plays drive direction. Possible values are "rightToLeft"
-     * or "leftToRight".
-     * @return {string}
-     */
-    driveDirection () {
-        if (this._obj.driveDirection === NFLPlayRecapVO.LEFT_TO_RIGHT) {
-            return NFLPlayRecapVO.LEFT_TO_RIGHT;
-        } else {
-            return NFLPlayRecapVO.RIGHT_TO_LEFT;
-        }
-    }
-
-    /**
-     * Returns true if the QB hands off the ball.
-     * @return {boolean}
-     */
-    isHandOff () {
-        return !this.isPassingPlay() && !this.isScramble();
-    }
-
-    /**
-     * Returns true if the QB sneeks the ball.
-     * @return {boolean}
-     */
-    isScramble () {
-        return this.isRushingPlay() && this._obj.rush.scramble;
-    }
-
-    /**
-     * Returns true if the pass results in an interception or fumble.
-     * @return {boolean}
-     */
-    isTurnover () {
-        return this.isPassingPlay() && this._obj.pass.intercepted;
-    }
-
-    /**
-     * Returns true if the QB passes the ball.
-     * @return {boolean}
-     */
-    isPassingPlay () {
-        return this.playType() === NFLPlayRecapVO.PASS;
-    }
-
-    /**
-     * Returns true if the play represents a run only.
-     * @return {boolean}
-     */
-    isRushingPlay () {
-        return this.playType() === NFLPlayRecapVO.RUSH;
-    }
+  /**
+   * Returns true if the play represents a run only.
+   * @return {boolean}
+   */
+  isRushingPlay() {
+    return this.playType() === NFLPlayRecapVO.RUSH;
+  }
 }
--- static/src/js/lib/live-animations/nfl/PassingPlayAnimation.js
+++ static/src/js/lib/live-animations/nfl/PassingPlayAnimation.js
@@ -5,6 +5,11 @@ import PassArrowAnimation from './PassArrowAnimation';
 import ReceptionAnimation from './ReceptionAnimation';
 import RushArrowAnimation from './RushArrowAnimation';
 import QuarterbackAnimation from './QuarterbackAnimation';
+import log from '../../logging.js';
+
+
+// get custom logger for actions
+const logLib = log.getLogger('lib');
 
 /**
  * Plays a pass play sequence by connecting a QB animation with a
@@ -12,20 +17,22 @@ import QuarterbackAnimation from './QuarterbackAnimation';
  * play recap.
  */
 export default class PassingPlayAnimation extends LiveAnimation {
-    
-    play (recap, field) {
-        let clips = [];
-        clips.push(() => new QuarterbackAnimation().play(recap, field));
-        clips.push(() => new PassArrowAnimation().play(recap, field));
-        clips.push(() => new ReceptionAnimation().play(recap, field));
-        clips.push(() => new RushArrowAnimation().play(recap, field));
-        clips.push(() => new DownLineAnimation().play(recap, field));
-        clips.push(() => new OutroAnimation().play(recap, field));
-        
-        //TODO: Load all clips fully before playback.
-        //TODO: Ensure the clip can be fully played before playing, or
-        //provide a way of clearing the current animation.
-        
-        return clips.reduce((p, fn) => p.then(fn), Promise.resolve());
-    }
+
+  play(recap, field) {
+    logLib.debug('PassingPlayAnimation.play', recap);
+
+    const clips = [];
+    clips.push(() => new QuarterbackAnimation().play(recap, field));
+    clips.push(() => new PassArrowAnimation().play(recap, field));
+    clips.push(() => new ReceptionAnimation().play(recap, field));
+    clips.push(() => new RushArrowAnimation().play(recap, field));
+    clips.push(() => new DownLineAnimation().play(recap, field));
+    clips.push(() => new OutroAnimation().play(recap, field));
+
+    // TODO: Load all clips fully before playback.
+    // TODO: Ensure the clip can be fully played before playing, or
+    // provide a way of clearing the current animation.
+
+    return clips.reduce((p, fn) => p.then(fn), Promise.resolve());
+  }
 }
