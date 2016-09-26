ca261ee34af7720987620a4dd6821b542d1e20b1
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-09-26 15:56:21 -0600

    Upgrading braintree-web, refactoring paypal checkout, super basic tests.

--- static/build/js/homepage.js
+++ static/build/js/homepage.js
@@ -1,2 +1,2 @@
-!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="/static/",r(0)}({0:function(t,r,e){e(1098),t.exports=e(825)},825:function(t,r){},1098:function(t,r,e){throw new Error("[HMR] Hot Module Replacement is disabled.")}});
+!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="/static/",r(0)}({0:function(t,r,e){e(1104),t.exports=e(827)},827:function(t,r){},1104:function(t,r,e){throw new Error("[HMR] Hot Module Replacement is disabled.")}});
 //# sourceMappingURL=homepage.js.map
