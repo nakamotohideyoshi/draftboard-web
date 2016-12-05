ca8ba8a3492f0b01f6f6754c4e27d5682b83b0b2
inlanger <inlanger@gmail.com>
2016-12-05 12:05:17 +0200

    update migration number to actual one's

--- account/migrations/0003_information_exclude_date.py
+++ account/migrations/0003_information_exclude_date.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0010_permissions'),
+        ('account', '0002_auto_20161017_1358'),
     ]
 
     operations = [
