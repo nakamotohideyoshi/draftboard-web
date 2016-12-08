dd2d2898f35d609373e83cd471cb4d6e2f21c517
inlanger <inlanger@gmail.com>
2016-12-08 12:47:02 +0200

    encoding update

--- account/tasks.py
+++ account/tasks.py
@@ -17,41 +17,35 @@ from account.models import (
     Information,
 )
 from contest.models import Entry
+from .utils import encode_uid
 # /password/reset/confirm/{uid}/{token}
-
-def encode_uid(pk):
-    try:
-        from django.utils.http import urlsafe_base64_encode
-        from django.utils.encoding import force_bytes
-        return urlsafe_base64_encode(force_bytes(pk)).decode()
-    except ImportError:
-        from django.utils.http import int_to_base36
-        return int_to_base36(pk)
 
 #
 # example password reset link
 # https://www.draftboard.com/api/account/password-reset-confirm/MjA0/47k-95ee193717cb75448cf0/
 
+
 @app.task(bind=True)
 def send_password_reset_email(self, user, https=True):
     #raise Exception('UNIMPLEMENTED - account.tasks.send_password_reset_email')
-    token       = default_token_generator.make_token(user)
-    uid         = encode_uid(user.pk)
-    site        = settings.SITE
-    protocol    = 'https' if https else 'http'
-    url         = '%s://%s/api/account/password-reset-confirm/%s/%s/' % (protocol, site, uid, token)
-    print( url )
+    token = default_token_generator.make_token(user)
+    uid = encode_uid(user.pk)
+    site = settings.SITE
+    protocol = 'https' if https else 'http'
+    url = '%s://%s/api/account/password-reset-confirm/%s/%s/' % (protocol, site, uid, token)
+    print(url)
     send_mail('password reset email', 'hey, heres your password reset link: ' + url, settings.DEFAULT_FROM_EMAIL, [user.email])
 
 
 @app.task(bind=True)
 def send_confirmation_email(self, user, https=True):
-    uid         = encode_uid(user.confirmation.pk)
-    site        = settings.SITE
-    protocol    = 'https' if https else 'http'
-    url         = '%s://%s/user-joined-confirm/%s/' % (protocol, site, uid)
-    print( url )
+    uid = encode_uid(str(user.confirmation.pk)+settings.ENCODE_SECRET_KEY)
+    site = settings.SITE
+    protocol = 'https' if https else 'http'
+    url = '%s://%s/%s' % (protocol, site, reverse('join-confirmation', kwargs={'uid': uid}))
+    print(url)
     send_mail('user confirmation', 'hey, you have joined the site, go to following link to confirm your registration: ' + url, settings.DEFAULT_FROM_EMAIL, [user.email])
+
 
 @app.task
 def inactive_users_email(users):
--- account/urls_non_api.py
+++ account/urls_non_api.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # django auth views we choose to have
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', auth_views.logout_then_login),
-    url(r'^user-joined-confirm/(?P<uid>.+)/$', ConfirmUserView.as_view()),
+    url(r'^user-joined-confirm/(?P<uid>.+)/$', ConfirmUserView.as_view(), name='join-confirmation'),
     url(r'^password/reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^password/reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
