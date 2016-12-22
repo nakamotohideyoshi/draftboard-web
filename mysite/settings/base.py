e5aabe45548e55426b87d67eb6a27d314720c3cd
inlanger <inlanger@gmail.com>
2016-12-22 16:59:13 +0200

    use python variable

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -432,6 +432,3 @@ COOKIE_ACCESS_DOMAIN = '.draftboard.com'
 # ----------------------------------------------------------
 KISS_ANALYTICS_CODE = 'db1235e51a9c6ea2d7bb2d3d1bdf93cbd2efaeff'
 GOOGLE_ANALYTICS_CODE = 'UA-85293033-1'
-
-# black list filename
-BLACK_LIST_FILE = 'black-list.txt'
