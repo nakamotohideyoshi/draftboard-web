a4c97a3616ba29467c0dd00814769b102f67a044
Zachary Wood <me@zacharywood.com>
2017-01-03 13:57:11 -0700

    update dev settings for staging

--- mysite/settings/staging.py
+++ mysite/settings/staging.py
@@ -1,19 +1,24 @@
-# settings.py for 'draftboard-dev' heroku app
+# settings.py for 'draftboard-staging' heroku app
 from .base import *
 from dj_database_url import config as heroku_db_config
 from urllib import parse
 
 # Constant for determining environment
-DOMAIN = 'dev.draftboard.com'
-ALLOWED_HOSTS = [DOMAIN]
+DOMAIN = 'staging.draftboard.com'
+ALLOWED_HOSTS = [DOMAIN, 'draftboard-staging.herokuapp.com']
 
 # Testing mode off
 DEBUG = False
 
 # Connect Heroku database
-# Based on https://devcenter.heroku.com/articles/python-concurrency-and-database-connections#number-of-active-connections
-# and Django 1.6 we can set 10 persistent connections bc we have a limit of 400 connections with our Premium 2 database.
-# 4 workers * up to 10 dynos * 10 connections = 400
+"""
+Based on: https://devcenter.heroku.com/articles/python-concurrency-and-database-connections#number-o
+f-active-connections
+
+And Django 1.6 we can set 10 persistent connections bc we have a limit of 400 connections with our
+Premium 2 database.
+4 workers * up to 10 dynos * 10 connections = 400
+"""
 # TODO django16 upgrade to persistent connections
 DATABASES = {
     'default': heroku_db_config()
@@ -21,10 +26,13 @@ DATABASES = {
 DATABASES['default']['autocommit'] = True
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
+DEFAULT_FROM_EMAIL = 'support+staging@draftboard.com'
+
 # SSL - we redirect all traffic to HTTPS at domain level, no need at application level
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # set this if all subdomains are under HTTPS
 
 # since we should have a heroku redis instance for production, override the default api cache name
 API_CACHE_NAME = 'api'
@@ -68,7 +76,6 @@ STATIC_URL = environ.get('DJANGO_STATIC_HOST', '') + '/static/'
 
 INSTALLED_APPS += (
     'gunicorn',  # gunicorn for Heroku
-    'raven.contrib.django.raven_compat',  # sentry for heroku
 )
 
 # Pusher
@@ -82,17 +89,15 @@ PAYPAL_CLIENT_ID = environ.get('PAYPAL_CLIENT_ID')
 PAYPAL_SECRET = environ.get('PAYPAL_SECRET')
 VZERO_ACCESS_TOKEN = environ.get('VZERO_ACCESS_TOKEN')
 
-# dataden mongo database connection
-# MONGO_SERVER_ADDRESS = environ.get('MONGO_SERVER_ADDRESS')   # ie: '123.132.123.123'
-# MONGO_AUTH_DB = environ.get('MONGO_AUTH_DB')          # 'admin'
-# MONGO_USER = environ.get('MONGO_USER')             # 'admin'
-# MONGO_PASSWORD = environ.get('MONGO_PASSWORD')         # 'dataden1'
-# MONGO_PORT = int(environ.get('MONGO_PORT'))         # 27017     cast MONGO_PORT to integer!
-# MONGO_HOST = environ.get('MONGO_HOST') % (MONGO_USER,
-#                                           MONGO_PASSWORD,
-#                                           MONGO_SERVER_ADDRESS,
-#                                           MONGO_PORT,
-#                                           MONGO_AUTH_DB)
+# Don't allow time travel on staging!
+DATETIME_DELTA_ENABLE = False
+
+# Inactive users
+INACTIVE_USERS_EMAILS = [
+    'pedro@runitonce.com',
+    'dan@runitonce.com',
+]
+
 
 MIDDLEWARE_CLASSES += (
     'account.middleware.access_subdomains.AccessSubdomainsMiddleware',
