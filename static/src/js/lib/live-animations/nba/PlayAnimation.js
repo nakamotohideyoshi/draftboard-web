5be6b5d1c87a151d9fbc730ebcaec8883d505be2
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-01-24 16:31:12 -0600

    Remove reference to debug propery on recap object.
    This puts the live-animations in permanent debug mode. We’ll need to go back in and tie this to an environment variable later on.

--- static/src/js/lib/live-animations/nba/PlayAnimation.js
+++ static/src/js/lib/live-animations/nba/PlayAnimation.js
@@ -5,18 +5,15 @@ import OutroAnimation from './OutroAnimation';
 import log from '../../../lib/logging';
 
 // get custom logger for actions
-const logComponent = log.getLogger('component');
+const logComponent = log.getLogger('live-animations');
 
 export default class PlayAnimation extends LiveAnimation {
 
   play(recap, court) {
-    logComponent.debug('liveNBAPlay.PlayAnimation.play', recap, court);
+    logComponent.debug('liveNBAPlay.PlayAnimation.play', recap.playType(), recap);
+
     const clips = [];
-
-    if (recap._obj.debug) {
-      clips.push(() => new DebugPlayAnimation().play(recap, court));
-    }
-
+    clips.push(() => new DebugPlayAnimation().play(recap, court));
     clips.push(() => new PlayerAnimation().play(recap, court));
     clips.push(() => new OutroAnimation().play(recap, court));
 
