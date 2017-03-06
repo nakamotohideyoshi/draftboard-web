a2895140470b838ba5a7b0b430dca12d76087d4d
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-03-06 12:51:41 -0600

    Better debugging values

--- static/src/js/components/live-debugger/live-debugger.jsx
+++ static/src/js/components/live-debugger/live-debugger.jsx
@@ -14,7 +14,7 @@ const stubMyLineup = {
   potentialWinnings: 10,
   rank: 1,
   timeRemaining: {
-    decimal: 1,
+    decimal: 0.91,
     duration: 50,
   },
 };
@@ -22,11 +22,11 @@ const stubMyLineup = {
 const stubOppLineup = {
   id: 3,
   name: 'Opponent Lineup',
-  fp: 10,
+  fp: 20,
   rank: 2,
   potentialWinnings: 10,
   timeRemaining: {
-    decimal: 1,
+    decimal: 0.91,
     duration: 50,
   },
   isLoading: false,
@@ -39,10 +39,41 @@ const stubOtherLineup = {
   rank: 3,
   potentialWinnings: 10,
   timeRemaining: {
-    decimal: 1,
+    decimal: 0.91,
     duration: 50,
   },
   isLoading: false,
+};
+
+const stubDebugLineup = {
+  id: 5,
+  name: 'Debug Lineup',
+  fp: 50,
+  rank: 4,
+  potentialWinnings: 10,
+  timeRemaining: {
+    decimal: 0.91,
+    duration: 50,
+  },
+  isLoading: false,
+};
+
+const stubContest = {
+  name: 'Debug Contest',
+  potentialWinnings: 10,
+  rank: 2,
+  isLoading: false,
+  hasLineupsUsernames: true,
+  lineupsUsernames: {
+    [stubMyLineup.id]: stubMyLineup.name,
+    [stubOppLineup.id]: stubOppLineup.name,
+    [stubOtherLineup.id]: stubOtherLineup.name,
+  },
+  rankedLineups: [
+    stubMyLineup.id,
+    stubOppLineup.id,
+    stubOtherLineup.id,
+  ],
 };
 
 const stubAvailableLineups = [stubMyLineup, stubOppLineup, stubOtherLineup];
@@ -51,24 +82,12 @@ const stubData = {
   watching: {
     sport: 'nba',
     contestId: 2,
-    opponentLineupId: null, // Null if you don't want to see the opponents overall-stats
+    myLineupId: stubMyLineup.id,
+    opponentLineupId: 3, // Null if you don't want to see the opponents overall-stats
   },
   currentEvent: null,
   eventsMultipart: {},
-  contest: {
-    name: 'Debug Contest',
-    potentialWinnings: 10,
-    rank: 2,
-    isLoading: false,
-    hasLineupsUsernames: true,
-    lineupsUsernames: {
-      [stubMyLineup.id]: stubMyLineup.name,
-      [stubOppLineup.id]: stubOppLineup.name,
-      [stubOtherLineup.id]: stubOtherLineup.name,
-    },
-//    rankedLineups: stubAvailableLineups.sort((a, b) => a.fp - b.fp).map(lineup => lineup.id),
-    rankedLineups: [2, 3, 4],
-  },
+  contest: stubContest,
   uniqueLineups: {
     lineups: stubAvailableLineups,
   },
