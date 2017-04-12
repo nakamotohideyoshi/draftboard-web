6b2ad47b17f30b3f2a53f28dd8cbb89bc2ceb1d7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-04-12 17:17:13 -0600

    new static build

--- static/build/js/homepage.js
+++ static/build/js/homepage.js
@@ -1,2 +1,2 @@
-!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="/static/",r(0)}({0:function(t,r,e){e(1164),t.exports=e(846)},846:function(t,r){},1164:function(t,r,e){throw new Error("[HMR] Hot Module Replacement is disabled.")}});
+!function(t){function r(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return t[o].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var e={};return r.m=t,r.c=e,r.p="/static/",r(0)}({0:function(t,r,e){e(1178),t.exports=e(853)},853:function(t,r){},1178:function(t,r,e){throw new Error("[HMR] Hot Module Replacement is disabled.")}});
 //# sourceMappingURL=homepage.js.map
