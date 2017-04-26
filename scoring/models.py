9d1f09431de8437f0bd43312d3361c1c5d54c128
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-04-26 11:54:52 -0600

    remove StatPoint.str_format field

--- scoring/models.py
+++ scoring/models.py
@@ -17,9 +17,6 @@ class StatPoint(models.Model):
     score_system = models.ForeignKey(ScoreSystem, null=False)
     stat = models.CharField(max_length=32, null=False, default='')
     value = models.FloatField(default=0.0, null=False)
-    # TODO:(zach) WTF does this field do? It appears to be nothing, but it makes it annoying to
-    # update a sport's stat points via the admin interface.
-    str_format = models.CharField(max_length=32, null=False, default='')
 
     class Meta:
         unique_together = ('score_system', 'stat')
