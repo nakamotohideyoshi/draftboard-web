81a880fe4495a5a689f42bd14605039ea5b78ed2
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-06-28 12:55:35 -0500

    Update KickReturn clip

---
+++ static/src/js/lib/live-animations/clips/nfl/kick-reception.js
@@ -0,0 +1,21 @@
+export const clip = {
+  frame_width: 190,
+  frame_height: 110,
+  length: 13,
+  registration_x: 90,
+  registration_y: 84,
+  avatars: [
+    {
+      name: 'receiver',
+      x: 96,
+      y: 32,
+      in: 13,
+    },
+  ],
+  data: {},
+  files: {
+    mine: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/kick-reception-blue.png'),
+    opponent: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/kick-reception-red.png'),
+    both: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/kick-reception-white.png'),
+  },
+};
