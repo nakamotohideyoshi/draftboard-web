42f9f6f19f30639d990074cd6b36f6b566942e8b
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-06-28 09:50:37 -0500

    Only display debug rects around clips when in live-debugger

--- static/src/js/lib/live-animations/nfl/animations/PlayerAnimation.js
+++ static/src/js/lib/live-animations/nfl/animations/PlayerAnimation.js
@@ -64,6 +64,10 @@ export default class PlayerAnimation extends LiveAnimation {
     clip.setPlayers(recap.players(), 'nfl');
 
     return clip.load(recap.whichSide()).then(() => {
+      if (window.is_debugging_live_animation) {
+        clip.debug();
+      }
+
       // Set the X position to where the player snaps the ball
       // by setting the initial position to the starting yard line.
       const yardline = this.getYardline(type, recap);
