dbe1d757d38ffe507e07f021812a69a588e6a37a
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-08-03 17:21:14 -0600

    new static build

--- static/build/js/homepage.js
+++ static/build/js/homepage.js
@@ -1,2 +1,2 @@
-!function(t){function r(e){if(o[e])return o[e].exports;var n=o[e]={exports:{},id:e,loaded:!1};return t[e].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var o={};return r.m=t,r.c=o,r.p="/static/",r(0)}({0:function(t,r,o){t.exports=o(1504)},1504:function(t,r){}});
+!function(t){function r(e){if(o[e])return o[e].exports;var n=o[e]={exports:{},id:e,loaded:!1};return t[e].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}var o={};return r.m=t,r.c=o,r.p="/static/",r(0)}({0:function(t,r,o){t.exports=o(1507)},1507:function(t,r){}});
 //# sourceMappingURL=homepage.js.map
