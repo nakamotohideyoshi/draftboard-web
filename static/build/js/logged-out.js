eed825b531b1a7c9a0d552a828893629a0699f47
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-08-09 09:24:00 -0500

    build static .JS

--- static/build/js/logged-out.js
+++ static/build/js/logged-out.js
@@ -1,2 +1,2 @@
-!function(t){function r(o){if(n[o])return n[o].exports;var e=n[o]={exports:{},id:o,loaded:!1};return t[o].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var n={};return r.m=t,r.c=n,r.p="/static/",r(0)}({0:function(t,r,n){t.exports=n(1502)},1502:function(t,r,n){"use strict";n(1508)},1508:function(t,r){}});
+!function(t){function r(o){if(n[o])return n[o].exports;var e=n[o]={exports:{},id:o,loaded:!1};return t[o].call(e.exports,e,e.exports,r),e.loaded=!0,e.exports}var n={};return r.m=t,r.c=n,r.p="/static/",r(0)}({0:function(t,r,n){t.exports=n(1549)},1549:function(t,r,n){"use strict";n(1555)},1555:function(t,r){}});
 //# sourceMappingURL=logged-out.js.map
