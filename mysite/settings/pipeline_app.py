dba8b74c21b7bb026d751c6053ed089637202158
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-08-09 16:59:19 -0600

    more pipeline settings

--- mysite/settings/pipeline_app.py
+++ mysite/settings/pipeline_app.py
@@ -10,3 +10,8 @@ ENVIRONMENT_COLOR = "#9ea548"
 DEFAULT_FROM_EMAIL = 'support+PIPELINE@draftboard.com'
 
 PUSHER_CHANNEL_PREFIX = 'pipeline_app__'
+
+# On our staging + prod apps celery has it's own redis DB but it's not possible to create
+# DBs programmatically so just use the same DB for both of these.
+REDIS_URL = environ.get('REDISCLOUD_OLIVE_URL')
+REDIS_URL_CELERY = environ.get('REDISCLOUD_OLIVE_URL')
