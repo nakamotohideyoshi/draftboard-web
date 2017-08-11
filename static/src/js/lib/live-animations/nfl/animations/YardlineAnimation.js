5de773177fb95096446ba08da28173545534675c
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-08-11 11:37:46 -0500

    Fix NFL kick return animation (#173)
    Leverages the new `Timeline` class for sequencing the receiver's "catch" cue point with the end of the ball's flight arrow.

--- static/src/js/lib/live-animations/nfl/animations/YardlineAnimation.js
+++ static/src/js/lib/live-animations/nfl/animations/YardlineAnimation.js
@@ -15,10 +15,11 @@ export default class YardlineAnimation extends LiveAnimation {
   play(recap, field, yardline, color = '#00FF00') {
     const marker = new YardLineMarker(field, yardline, color);
     const markerEl = field.addChild(marker.el, 0, 0, 1);
+    markerEl.style.opacity = 0;
 
     return new Promise(resolve => {
-      TweenLite.from(markerEl, 0.25, {
-        opacity: 0,
+      TweenLite.to(markerEl, 0.5, {
+        opacity: 1,
         ease: 'none',
         onComplete: () => resolve(),
       });
