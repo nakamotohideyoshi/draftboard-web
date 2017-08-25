ae4fef144b1ad84666e42eb529cf28b68b15e2ca
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-08-25 10:53:07 -0500

    Add initial support for “white” handoffs and scrambles

--- static/src/js/lib/live-animations/nfl/clips/qb-shotgun-handoff-short-right.js
+++ static/src/js/lib/live-animations/nfl/clips/qb-shotgun-handoff-short-right.js
@@ -15,5 +15,6 @@ export const clip = {
   files: {
     mine: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-handoff-right-blue.png'),
     opponent: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-handoff-right-red.png'),
+    none: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-handoff-right-red.png'),
   },
 };
--- static/src/js/lib/live-animations/nfl/clips/qb-shotgun-scramble.js
+++ static/src/js/lib/live-animations/nfl/clips/qb-shotgun-scramble.js
@@ -15,5 +15,6 @@ export const clip = {
   files: {
     mine: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-scramble-blue.png'),
     opponent: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-scramble-red.png'),
+    none: require('../../../../../img/blocks/live-animation-stage/nfl/sequences/qb-shotgun-scramble-red.png'),
   },
 };
