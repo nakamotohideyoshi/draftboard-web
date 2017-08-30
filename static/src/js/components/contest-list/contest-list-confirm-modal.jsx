0473833f4a52f594462701c2b6d70e17720620b9
Parker Hutchinson <hutchinsonparker@gmail.com>
2017-08-30 16:30:50 -0700

    so conviluted, api is to blame and going on my naughty list. got a blacklist created for all the redundent key values passing into player_stats. got it returning items with values greater than 0. need to create a special object map of positions with default zero'd values for when the player_stats is empty. that could be easier if the api returned an object even if all stats are 0. got the labels printing based on key names.

--- static/src/js/components/contest-list/contest-list-confirm-modal.jsx
+++ static/src/js/components/contest-list/contest-list-confirm-modal.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import EnterContestButton from './enter-contest-button.jsx';
 import AppStateStore from '../../stores/app-state-store.js';
 import { humanizeCurrency } from '../../lib/utils/currency.js';
-import log from '../../lib/logging';
+// import log from '../../lib/logging';
 
 /**
  * When a user attempts to enter a contest, prompt them to confirm.
