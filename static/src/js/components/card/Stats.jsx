a7e87a76b0cec9abb34d5fc9ffc73a7422134853
Parker Hutchinson <hutchinsonparker@gmail.com>
2017-08-30 15:26:35 -0700

    some serious magic going on to make this stats section work as expected.

--- static/src/js/components/card/Stats.jsx
+++ static/src/js/components/card/Stats.jsx
@@ -4,33 +4,31 @@ import PropTypes from 'prop-types';
 class Stats extends Component {
   getStatItems() {
     const statItems = [];
-
-    for (const key in this.props) {
-      if (this.props.hasOwnProperty(key)) {
-        const item = key;
-        let value = this.props[key];
-
-        // add $ sign if its needed
-        if (
-          item === 'remsalary' ||
-          item === 'fees' ||
-          item === 'playeravg') {
-          value = `$${value}`;
-        }
-
-        if (item !== 'children') {
-          statItems.push([
-            <dd key={item} className={`card-${item}`}>
-              <dl>
-                <dt>{item}</dt>
-                <dd ref={key} >{value}</dd>
-              </dl>
-            </dd>,
-          ]);
+    for (let i = 0; i < this.props.player_stats.length; i++) {
+      for (const key in this.props.player_stats[i]) {
+        if (this.props.player_stats[i].hasOwnProperty(key)) {
+          const item = key;
+          let value = this.props.player_stats[i][key];
+          // add $ sign if its needed
+          if (
+            item === 'remsalary' ||
+            item === 'fees' ||
+            item === 'playeravg') {
+            value = `$${value}`;
+          }
+          if (item !== 'children') {
+            statItems.push([
+              <dd key={item} className={`card-${item}`}>
+                <dl>
+                  <dt>{item}</dt>
+                  <dd ref={key} >{value}</dd>
+                </dl>
+              </dd>,
+            ]);
+          }
         }
       }
     }
-
     return statItems;
   }
   render() {
@@ -44,6 +42,7 @@ class Stats extends Component {
 
 Stats.propTypes = {
   children: PropTypes.element,
+  player_stats: PropTypes.array,
 };
 
 export default Stats;
